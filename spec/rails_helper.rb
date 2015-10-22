ministryofjustice/case_file_editor

Configure for feature testing

spec/rails_helper.rb
@@ -1,9 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+Capybara.default_driver = :poltergeist
+Capybara.default_wait_time = 3
+Capybara.asset_host = 'http://localhost:3000'
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true

ministryofjustice/prison-visits-2

spec/rails_helper.rb
@@ -1,9 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+Capybara.default_driver = :poltergeist
+Capybara.default_max_wait_time = 3
+Capybara.asset_host = 'http://localhost:3000'
 
 ActiveRecord::Migration.maintain_test_schema!
 
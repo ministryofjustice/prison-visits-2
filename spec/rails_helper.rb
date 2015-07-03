e066012873de0eddd91d4a7f0b776c0ea9cfc40c
Paul Battley <pbattley@gmail.com>
2015-07-03 17:44:12 +0100

    Configure for feature testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+Capybara.default_driver = :poltergeist
+Capybara.default_max_wait_time = 3
+Capybara.asset_host = 'http://localhost:3000'
 
 ActiveRecord::Migration.maintain_test_schema!
 
