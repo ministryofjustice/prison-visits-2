4028dbde9f6190164d02a34c51eb927fc8825e99
Paul Battley <pbattley@gmail.com>
2015-10-26 15:53:11 +0000

    Import DateHelper from previous app
    This is simplified now that we aren't passing around things that might
    be strings and makes more use of the I18n infrastructure.

---
+++ app/helpers/date_helper.rb
@@ -0,0 +1,56 @@
+module DateHelper
+  def format_date_of_birth(date)
+    I18n.l(date, format: :date_of_birth)
+  end
+
+  def format_date_of_visit(date)
+    I18n.l(date, format: :date_of_visit)
+  end
+
+  def format_time_12hr(time)
+    I18n.l(time, format: :twelve_hour)
+  end
+
+  def format_time_24hr(time)
+    I18n.l(time, format: :twenty_four_hour)
+  end
+
+  def format_slot_begin_time_for_public(slot)
+    I18n.t(
+      'formats.slot.public.begin_only',
+      date: format_date_of_visit(slot.begin_at.to_date),
+      time: format_time_12hr(slot.begin_at)
+    )
+  end
+
+  def format_slot_for_public(slot)
+    I18n.t(
+      'formats.slot.public.full',
+      date: format_date_of_visit(slot.begin_at.to_date),
+      time: format_time_12hr(slot.begin_at),
+      duration: format_duration(slot.duration)
+    )
+  end
+
+  def format_slot_for_staff(slot)
+    I18n.t(
+      'formats.slot.staff',
+      date: format_date_of_visit(slot.begin_at.to_date),
+      begin: format_time_24hr(slot.begin_at),
+      end: format_time_24hr(slot.end_at)
+    )
+  end
+
+private
+
+  def format_duration(secs)
+    hours = secs.to_i / 3600
+    minutes = (secs.to_i / 60) % 60
+
+    parts = []
+    parts << I18n.t('formats.duration.hours', count: hours) if hours > 0
+    parts << I18n.t('formats.duration.minutes', count: minutes) if minutes > 0
+
+    parts.join(I18n.t('formats.duration.glue'))
+  end
+end
