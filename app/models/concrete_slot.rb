67a5207caa98c39f8b5c22f776d1f248dfc8aa64
Paul Battley <pbattley@gmail.com>
2015-10-26 13:59:56 +0000

    Add begin_at/end_at/duration to ConcreteSlot

--- app/models/concrete_slot.rb
+++ app/models/concrete_slot.rb
@@ -17,4 +17,24 @@ ConcreteSlot = Struct.new(
       year, month, day, begin_hour, begin_minute, end_hour, end_minute
     ]
   end
+
+  # We are explicitly parsing these as UTC, but this Rubocop cop isn't clever
+  # enough to realise. We use UTC because we don't actually care about time
+  # zone offsets: booking times are always given in terms of wall time, and
+  # we only use Time to give us a convenient way to perform maths and to
+  # format dates and times for output.
+  #
+  # rubocop:disable Rails/TimeZone
+  def begin_at
+    Time.new(year, month, day, begin_hour, begin_minute, 0, '+00:00')
+  end
+
+  def end_at
+    Time.new(year, month, day, end_hour, end_minute, 0, '+00:00')
+  end
+  # rubocop:enable Rails/TimeZone
+
+  def duration
+    (end_at - begin_at).to_i
+  end
 end
