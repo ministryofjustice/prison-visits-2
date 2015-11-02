ba2304c580ce54eb0b887d65e46a753e9edb6f78
Todd Tyree <tatyree@gmail.com>
2015-11-02 13:26:43 +0000

    Remove .spam_reported? blocks in original spec
    The ported sendgrid_api_spec is nearly 400 lines long and somewhat
    confusing. This breaks out the .spam_reported? method into an individual
    spec that is smaller and conceptually cleaner.

--- spec/services/sendgrid_api_spec.rb
+++ spec/services/sendgrid_api_spec.rb
@@ -14,74 +14,6 @@ RSpec.describe SendgridApi do
       Rails.configuration.action_mailer.smtp_settings = smtp_settings
     end
 
-    describe '.spam_reported?' do
-      let(:body) { '[]' }
-
-      before do
-        stub_request(:post, 'https://api.sendgrid.com/api/spamreports.get.json').
-          with(query: hash_including(
-            'api_key'   => 'test_smtp_password',
-            'api_user'  => 'test_smtp_username',
-            'email'     => 'test@example.com')).
-          to_return(status: 200, body: body, headers: {})
-      end
-
-      context 'error handling' do
-        context 'when the API raises an exception' do
-          before do
-            stub_request(:post, 'https://api.sendgrid.com/api/spamreports.get.json').
-              to_raise(StandardError)
-          end
-
-          it 'has no spam report' do
-            expect(subject.spam_reported?('test@example.com')).to be_falsey
-          end
-        end
-
-        context 'when the API reports an error' do
-          let(:body) { '{"error":"LOL"}' }
-
-          it 'has no spam report' do
-            expect(subject.spam_reported?('test@example.com')).to be_falsey
-          end
-        end
-
-        context 'when the API returns non-JSON' do
-          let(:body) { 'Oopsy daisy' }
-
-          it 'has no spam report' do
-            expect(subject.spam_reported?('test@example.com')).to be_falsey
-          end
-        end
-      end
-
-      context 'when no error' do
-        context 'when there is no spam report' do
-          let(:body) { '[]' }
-
-          it 'has no spam report' do
-            expect(subject.spam_reported?('test@example.com')).to be_falsey
-          end
-        end
-
-        context 'when there is a spam report' do
-          let(:body) {
-            %([
-              {
-                "ip": "174.36.80.219",
-                "email": "test@example.com",
-                "created": "2009-12-06 15:45:08"
-              }
-            ])
-          }
-
-          it 'has a spam report' do
-            expect(subject.spam_reported?('test@example.com')).to be_truthy
-          end
-        end
-      end
-    end
-
     describe '.bounced?' do
       let(:body) { '[]' }
 
@@ -298,17 +230,6 @@ RSpec.describe SendgridApi do
       it 'does not talk to sendgrid' do
         expect(HTTParty).to receive(:post).never
         subject.bounced?('test@example.com')
-      end
-    end
-
-    describe '.spam_reported?' do
-      it 'never says that the email address has been reported for spam' do
-        expect(subject.spam_reported?('test@example.com')).to be_falsey
-      end
-
-      it 'does not talk to sendgrid' do
-        expect(HTTParty).to receive(:post).never
-        subject.spam_reported?('test@example.com')
       end
     end
 
