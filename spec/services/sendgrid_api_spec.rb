a730d53673952caa3c4d57785cef8fd88b192777
Todd Tyree <tatyree@gmail.com>
2015-11-02 13:32:57 +0000

    Remove .remove_from_spam_list in original spec
    The ported sendgrid_api_spec is nearly 400 lines long and somewhat
    confusing. This breaks out the .remove_from_spam_list method into an
    individual spec that is smaller and conceptually cleaner.

--- spec/services/sendgrid_api_spec.rb
+++ spec/services/sendgrid_api_spec.rb
@@ -13,71 +13,6 @@ RSpec.describe SendgridApi do
       example.run
       Rails.configuration.action_mailer.smtp_settings = smtp_settings
     end
-
-    describe '.remove_from_spam_list' do
-      let(:body) { nil }
-
-      before do
-        stub_request(:post, 'https://api.sendgrid.com/api/spamreports.delete.json').
-          with(query: hash_including(
-            'api_key'   => 'test_smtp_password',
-            'api_user'  => 'test_smtp_username',
-            'email'     => 'test@example.com')).
-          to_return(status: 200, body: body, headers: {})
-      end
-
-      context 'error handling' do
-        context 'when the API raises an exception' do
-          before do
-            stub_request(:post, 'https://api.sendgrid.com/api/spamreports.delete.json').
-              with(query: hash_including(
-                'api_key'   => 'test_smtp_password',
-                'api_user'  => 'test_smtp_username',
-                'email'     => 'test@example.com')).
-              to_raise(StandardError)
-          end
-
-          specify do
-            expect { subject.remove_from_spam_list('test@example.com') }.
-              to raise_error(StandardError)
-          end
-        end
-
-        context 'when the API reports an error' do
-          let(:body) { '{"error":"LOL"}' }
-
-          specify do
-            expect { subject.remove_from_spam_list('test@example.com') }.
-              to raise_error(SendgridToolkit::APIError)
-          end
-        end
-
-        context 'when the API returns non-JSON' do
-          let(:body) { 'Oopsy daisy' }
-
-          specify do
-            expect { subject.remove_from_spam_list('test@example.com') }.
-              to raise_error(JSON::ParserError)
-          end
-        end
-      end
-
-      context 'when there is no bounce' do
-        let(:body) { '{"message": "Email does not exist"}' }
-
-        specify do
-          expect(subject.remove_from_spam_list('test@example.com')).to be_falsey
-        end
-      end
-
-      context 'when there is a bounce' do
-        let(:body) { '{"message": "success"}' }
-
-        it 'removes it' do
-          expect(subject.remove_from_spam_list('test@example.com')).to be_truthy
-        end
-      end
-    end
   end
 
   context 'without sendgrid configured' do
@@ -86,17 +21,6 @@ RSpec.describe SendgridApi do
       Rails.configuration.action_mailer.smtp_settings = {}
       example.run
       Rails.configuration.action_mailer.smtp_settings = smtp_settings
-    end
-
-    describe '.remove_from_spam_list' do
-      specify do
-        expect(subject.remove_from_spam_list('test@example.com')).to be_falsey
-      end
-
-      it 'does not talk to sendgrid' do
-        expect(HTTParty).to receive(:post).never
-        subject.remove_from_spam_list('test@example.com')
-      end
     end
   end
 end
