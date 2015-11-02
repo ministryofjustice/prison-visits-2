0549c87ec556a25bce69b5aaf32cb7b778f61893
Todd Tyree <tatyree@gmail.com>
2015-11-02 13:31:19 +0000

    Remove .remove_from_bounce_list in spec
    The ported sendgrid_api_spec is nearly 400 lines long and somewhat
    confusing. This breaks out the .remove_from_bounce_list method into an
    individual spec that is smaller and conceptually cleaner.

--- spec/services/sendgrid_api/remove_from_bounce_list_spec.rb
+++ spec/services/sendgrid_api/remove_from_bounce_list_spec.rb
@@ -5,18 +5,27 @@ require_relative './shared_examples'
 RSpec.describe SendgridApi, '.remove_from_bounce_list' do
   subject {
     described_class.remove_from_bounce_list('test@example.com')
+            expect { subject.remove_from_bounce_list('test@example.com') }.
   }
+        let(:body) { '{"message": "Email does not exist"}' }
 
   context 'sendgrid credentials are set' do
     include_examples 'error handling'
+      context 'when there is a bounce' do
+        let(:body) { '{"message": "success"}' }
 
     context 'when there is no bounce' do
       include_examples 'API reports email does not exist'
+        it 'removes it' do
+          expect(subject.remove_from_bounce_list('test@example.com')).to be_truthy
     end
 
     context 'when there is a bounce' do
       describe 'it removes it' do
         include_examples 'API reports success'
+      it 'does not talk to sendgrid' do
+        expect(HTTParty).to receive(:post).never
+        subject.remove_from_bounce_list('test@example.com')
       end
     end
 
