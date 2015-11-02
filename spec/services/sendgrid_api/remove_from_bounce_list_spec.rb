6e003fe379b77d3e186119cb546cd10aad4bac63
Todd Tyree <tatyree@gmail.com>
2015-11-02 13:31:19 +0000

    Remove .remove_from_bounce_list in spec
    The ported sendgrid_api_spec is nearly 400 lines long and somewhat
    confusing. This breaks out the .remove_from_bounce_list method into an
    individual spec that is smaller and conceptually cleaner.

---
+++ spec/services/sendgrid_api/remove_from_bounce_list_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+require_relative '../sendgrid_api_shared_context'
+
+RSpec.describe SendgridApi, '.remove_from_bounce_list' do
+  subject { described_class.new }
+
+  context 'with sendgrid configured' do
+    describe '.remove_from_bounce_list' do
+      let(:body) { nil }
+      include_context 'sendgrid is configured'
+      include_context 'sendgrid api responds normally'
+
+      context 'error handling' do
+        context 'when the API raises an exception' do
+          include_context 'sendgrid api raises an exception'
+
+          specify do
+            expect { subject.remove_from_bounce_list('test@example.com') }.
+              to raise_error(StandardError)
+          end
+        end
+
+        context 'when the API reports an error' do
+          let(:body) { '{"error":"LOL"}' }
+
+          specify do
+            expect { subject.remove_from_bounce_list('test@example.com') }.
+              to raise_error(SendgridToolkit::APIError)
+          end
+        end
+
+        context 'when the API returns non-JSON' do
+          let(:body) { 'Oopsy daisy' }
+
+          specify do
+            expect { subject.remove_from_bounce_list('test@example.com') }.
+              to raise_error(JSON::ParserError)
+          end
+        end
+      end
+
+      context 'when there is no bounce' do
+        let(:body) { '{"message": "Email does not exist"}' }
+
+        specify do
+          expect(subject.remove_from_bounce_list('test@example.com')).to be_falsey
+        end
+      end
+
+      context 'when there is a bounce' do
+        let(:body) { '{"message": "success"}' }
+
+        it 'removes it' do
+          expect(subject.remove_from_bounce_list('test@example.com')).to be_truthy
+        end
+      end
+    end
+  end
+
+  context 'without sendgrid configured' do
+    include_context 'sendgrid is not configured'
+    describe '.remove_from_bounce_list' do
+      specify do
+        expect(subject.remove_from_bounce_list('test@example.com')).to be_falsey
+      end
+
+      it 'does not talk to sendgrid' do
+        expect(HTTParty).to receive(:post).never
+        subject.remove_from_bounce_list('test@example.com')
+      end
+    end
+  end
+end
