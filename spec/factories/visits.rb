331c7b66b90e4c7e3ce67002fe7455ff4e028f3c
Todd Tyree <tatyree@gmail.com>
2015-11-09 15:46:42 +0000

    Create a factory to represent a valid visit
    This is to ease building test data. In this instance it is for the visit
    mailer, but it has wide application beyond that use-case.

---
+++ spec/factories/visits.rb
@@ -0,0 +1,40 @@
+FactoryGirl.define do
+  factory :visit do
+    prison
+    prisoner_first_name do
+      FFaker::Name.first_name
+    end
+
+    prisoner_last_name do
+      FFaker::Name.last_name
+    end
+
+    prisoner_date_of_birth '1970-01-01'
+    prisoner_number do |n|
+      'ABC%04d' % n
+    end
+
+    visitor_first_name do
+      FFaker::Name.first_name
+    end
+
+    visitor_last_name do
+      FFaker::Name.last_name
+    end
+
+    visitor_date_of_birth '1980-01-10'
+
+    visitor_email_address do
+      FFaker::Internet.disposable_email
+    end
+
+    visitor_phone_no do
+      FFaker::PhoneNumber.short_phone_number
+    end
+
+    slot_option_1 do |v|
+      v.prison.available_slots.first.iso8601
+    end
+    processing_state 'start'
+  end
+end
