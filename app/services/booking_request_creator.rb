bae66359aba644004c12bed481ea2d976d52906d
Paul Battley <pbattley@gmail.com>
2015-11-09 16:29:37 +0000

    Refactor BookingRequestCreator a little
    Params is a better name than options.

--- app/services/booking_request_creator.rb
+++ app/services/booking_request_creator.rb
@@ -1,17 +1,20 @@
 class BookingRequestCreator
   def create!(prisoner_step, visitors_step, slots_step)
-    options = [
-      prisoner_step_options(prisoner_step),
-      visitors_step_options(visitors_step),
-      slots_step_options(slots_step)
-    ].inject(&:merge)
-
-    Visit.create! options
+    params = build_params(prisoner_step, visitors_step, slots_step)
+    Visit.create!(params)
   end
 
 private
 
-  def prisoner_step_options(step)
+  def build_params(prisoner_step, visitors_step, slots_step)
+    [
+      prisoner_step_params(prisoner_step),
+      visitors_step_params(visitors_step),
+      slots_step_params(slots_step)
+    ].inject(&:merge)
+  end
+
+  def prisoner_step_params(step)
     {
       prison_id: step.prison_id,
       prisoner_first_name: step.first_name,
@@ -21,7 +24,7 @@ private
     }
   end
 
-  def visitors_step_options(step)
+  def visitors_step_params(step)
     {
       visitor_first_name: step.first_name,
       visitor_last_name: step.last_name,
@@ -31,7 +34,7 @@ private
     }
   end
 
-  def slots_step_options(step)
+  def slots_step_params(step)
     {
       slot_option_1: step.option_1,
       slot_option_2: step.option_2,
