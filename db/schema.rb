02696078d0654d520271e0553377290c0696a359
Paul Battley <pbattley@gmail.com>
2015-11-17 16:33:29 +0000

    Restore foreign key enforcement
    Data integrity is your friend.
    
    This was inadvertently lost in the move to UUID table IDs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112112210) do
+ActiveRecord::Schema.define(version: 20151117162316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,4 +64,6 @@ ActiveRecord::Schema.define(version: 20151112112210) do
 
   add_index "visits", ["prison_id"], name: "index_visits_on_prison_id", using: :btree
 
+  add_foreign_key "additional_visitors", "visits"
+  add_foreign_key "visits", "prisons"
 end
