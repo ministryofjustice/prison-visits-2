3117ee0d1870d9fe5f8d766c3006105be60ee651
Paul Battley <pbattley@gmail.com>
2015-11-23 08:52:42 +0000

    Match text in email across line breaks
    This treats all whitespace as a simple space, which is the technique
    used by Capybara.

--- spec/support/matchers/email_matchers.rb
+++ spec/support/matchers/email_matchers.rb
@@ -4,11 +4,15 @@ RSpec::Matchers.define :receive_email do
       ActionMailer::Base.deliveries.select { |e| e.to.include?(address) }
 
     emails.select! { |e| e.subject.match(subject) } if subject
-    emails.select! { |e| e.text_part.body.match(body) } if body
+    emails.select! { |e| body_matches?(e, body) } if body
     emails.any?
   end
 
   chain :with_subject, :subject
   chain :with_body, :body
   chain :and_body, :body
+
+  def body_matches?(email, pattern)
+    email.text_part.body.to_s.gsub(/\s+/, ' ').match(pattern)
+  end
 end
