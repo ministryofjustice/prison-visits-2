77bce5cd8f132bf0134ca3e6a4e8cab20de1e2f1
Paul Battley <pbattley@gmail.com>
2015-11-23 14:30:14 +0000

    Make BookingResponder spec intention clearer
    Move specs for generic behaviour out of slot-specific blocks.

--- spec/services/booking_responder_spec.rb
+++ spec/services/booking_responder_spec.rb
@@ -27,46 +27,46 @@ RSpec.describe BookingResponder do
       booking_response.reference_no = '1337807'
     end
 
+    it 'changes the status of the visit to booked' do
+      expect(visit_after_responding).to be_booked
+    end
+
+    it 'sets the reference number of the visit' do
+      expect(visit_after_responding.reference_no).to eq('1337807')
+    end
+
+    it 'marks the visit as closed' do
+      booking_response.closed_visit = true
+      expect(visit_after_responding).to be_closed
+    end
+
+    it 'marks the visit as not closed' do
+      booking_response.closed_visit = false
+      expect(visit_after_responding).not_to be_closed
+    end
+
+    it 'emails the visitor' do
+      expect(VisitorMailer).to receive(:booked).with(visit).
+        and_return(mailing)
+      expect(mailing).to receive(:deliver_later)
+      subject.respond!
+    end
+
+    it 'emails the prison' do
+      expect(PrisonMailer).to receive(:booked).with(visit).
+        and_return(mailing)
+      expect(mailing).to receive(:deliver_later)
+      subject.respond!
+    end
+
     context 'with the first slot' do
       before do
         booking_response.selection = 'slot_0'
       end
 
-      it 'changes the status of the visit.reload to booked' do
-        expect(visit_after_responding).to be_booked
-      end
-
-      it 'sets the reference number of the visit' do
-        expect(visit_after_responding.reference_no).to eq('1337807')
-      end
-
       it 'assigns the selected slot' do
         expect(visit_after_responding.slot_granted).
           to eq(visit_after_responding.slots[0])
-      end
-
-      it 'marks the visit as closed' do
-        booking_response.closed_visit = true
-        expect(visit_after_responding).to be_closed
-      end
-
-      it 'marks the visit as not closed' do
-        booking_response.closed_visit = false
-        expect(visit_after_responding).not_to be_closed
-      end
-
-      it 'emails the visitor' do
-        expect(VisitorMailer).to receive(:booked).with(visit).
-          and_return(mailing)
-        expect(mailing).to receive(:deliver_later)
-        subject.respond!
-      end
-
-      it 'emails the prison' do
-        expect(PrisonMailer).to receive(:booked).with(visit).
-          and_return(mailing)
-        expect(mailing).to receive(:deliver_later)
-        subject.respond!
       end
     end
 
