e38fcbd224807760bff465167a433185bb5b03bd
Paul Battley <pbattley@gmail.com>
2015-11-24 14:41:52 +0000

    Consider lead time in slot availability
    It is only possible to book a slot from the day after the confirmation
    date. The latter is calculated from the prison's lead days, taking into
    account weekends (where appropriate) and public holidays.

--- spec/models/prison_spec.rb
+++ spec/models/prison_spec.rb
@@ -1,41 +1,144 @@
 require 'rails_helper'
 
 RSpec.describe Prison, type: :model do
-  it 'enumerates available slots within booking window starting tomorrow' do
-    subject.slot_details = {
-      'recurring' => {
-        'mon' => ['1001-1100'],
-        'tue' => ['1002-1100'],
-        'wed' => ['1003-1100'],
-        'thu' => ['1004-1100'],
-        'fri' => ['1005-1100'],
-        'sat' => ['1006-1100'],
-        'sun' => ['1007-1100']
+  before do
+    subject.lead_days = 3
+    subject.weekend_processing = false
+  end
+
+  describe 'first_bookable_date' do
+    #
+    #    Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo
+    #     1  2  3  4  5  6  7  8  9 10 11 12
+    #     T  1  .  .  2  3  *  *  *  .  .  *
+    #     |              |  |<--------->|
+    #   Today       Confirm    Bookable
+
+    let(:today) { Date.new(2015, 10, 1) }
+    let(:wednesday) { Date.new(2015, 10, 7) }
+
+    it 'is the day after the lead time' do
+      expect(subject.first_bookable_date(today)).to eq(wednesday)
+    end
+  end
+
+  describe 'last_bookable_date' do
+    it 'is [booking_window] full days from tomorrow' do
+      subject.booking_window = 10
+      today = Date.new(2015, 10, 1)
+      expect(subject.last_bookable_date(today)).to eq(Date.new(2015, 10, 11))
+    end
+  end
+
+  describe 'available_slots' do
+    it 'enumerates available slots within booking window starting after lead time' do
+      #
+      #    Th Fr Sa Su Mo Tu We Th Fr Sa Su Mo
+      #     1  2  3  4  5  6  7  8  9 10 11 12
+      #     T  1  .  .  2  3  *  *  *  .  .  *
+      #     |              |  |<--------->|
+      #   Today       Confirm    Bookable
+
+      subject.slot_details = {
+        'recurring' => {
+          'mon' => ['1001-1100'],
+          'tue' => ['1002-1100'],
+          'wed' => ['1003-1100'],
+          'thu' => ['1004-1100'],
+          'fri' => ['1005-1100'],
+          'sat' => ['1006-1100'],
+          'sun' => ['1007-1100']
+        }
       }
-    }
-    subject.booking_window = 5
-    today = Date.new(2015, 10, 1) # Thursday
-    expect(subject.available_slots(today).to_a).to eq(
-      [
-        ConcreteSlot.new(2015, 10, 2, 10, 5, 11, 0),
-        ConcreteSlot.new(2015, 10, 3, 10, 6, 11, 0),
-        ConcreteSlot.new(2015, 10, 4, 10, 7, 11, 0),
-        ConcreteSlot.new(2015, 10, 5, 10, 1, 11, 0),
-        ConcreteSlot.new(2015, 10, 6, 10, 2, 11, 0)
-      ]
-    )
+      subject.booking_window = 10
+      today = Date.new(2015, 10, 1) # Thursday
+      expect(subject.available_slots(today).to_a).to eq(
+        [
+          ConcreteSlot.new(2015, 10,  7, 10, 3, 11, 0),
+          ConcreteSlot.new(2015, 10,  8, 10, 4, 11, 0),
+          ConcreteSlot.new(2015, 10,  9, 10, 5, 11, 0),
+          ConcreteSlot.new(2015, 10, 10, 10, 6, 11, 0),
+          ConcreteSlot.new(2015, 10, 11, 10, 7, 11, 0)
+        ]
+      )
+    end
   end
 
-  it 'starts bookings tomorrow' do
-    # Bookable days are 2, 3, ...
-    today = Date.new(2015, 10, 1)
-    expect(subject.first_bookable_date(today)).to eq(Date.new(2015, 10, 2))
-  end
+  describe 'confirm_by' do
+    context 'when the lead days are not broken up by a holiday or weekend' do
+      #
+      #    16 17 18 19 20 21 22 23 24 25 26 27 28 29
+      #    Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su
+      #     T  1  2  3  *  .  .  *  *  *  *  *  .  .
+      #     |        |
+      #   Today   Confirm
 
-  it 'ends bookings at the end of the booking window, starting tomorrow' do
-    # Bookable days are 2, 3, 4, 5, 6
-    subject.booking_window = 5
-    today = Date.new(2015, 10, 1)
-    expect(subject.last_bookable_date(today)).to eq(Date.new(2015, 10, 6))
+      let(:thursday) { Date.new(2015, 11, 19) }
+      let(:monday) { Date.new(2015, 11, 16) }
+
+      it 'returns a date [lead days] days from now' do
+        expect(subject.confirm_by(monday)).to eq(thursday)
+      end
+    end
+
+    context 'when the lead days are broken up by the weekend' do
+      context 'when a prison doesn’t process at the weekend' do
+        #
+        #    16 17 18 19 20 21 22 23 24 25 26 27 28 29
+        #    Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su
+        #     *  *  *  T  1  .  .  2  3  *  *  *  .  .
+        #              |              |
+        #            Today         Confirm
+
+        let(:thursday) { Date.new(2015, 11, 19) }
+        let(:tuesday) { Date.new(2015, 11, 24) }
+
+        it 'skips Saturday and Sunday' do
+          expect(subject.confirm_by(thursday)).to eq tuesday
+        end
+      end
+
+      context 'when a prison processes visits at the weekend' do
+        #
+        #    16 17 18 19 20 21 22 23 24 25 26 27 28 29
+        #    Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su
+        #     *  *  *  T  1  2  3  *  *  *  *  *  *  *
+        #              |        |
+        #            Today   Confirm
+
+        let(:thursday) { Date.new(2015, 11, 19) }
+        let(:sunday) { Date.new(2015, 11, 22) }
+
+        before do
+          subject.weekend_processing = true
+        end
+
+        it 'includes Saturday and Sunday' do
+          expect(subject.confirm_by(thursday)).to eq(sunday)
+        end
+      end
+    end
+
+    context 'when the lead days are broken up by a public holiday' do
+      #
+      #    16 17 18 19 20 21 22 23 24 25 26 27 28 29
+      #    Mo Tu We Th Fr Sa Su Mo Tu We Th Fr Sa Su
+      #     *  *  *  T  1  .  .  H  2  3  *  *  .  .
+      #              |                 |
+      #            Today            Confirm
+
+      let(:thursday) { Date.new(2015, 11, 19) }
+      let(:monday) { Date.new(2015, 11, 23) }
+      let(:wednesday) { Date.new(2015, 11, 25) }
+
+      before do
+        allow(Rails.configuration).to receive(:holidays).
+          and_return([monday])
+      end
+
+      it 'skips the public holiday' do
+        expect(subject.confirm_by(thursday)).to eq(wednesday)
+      end
+    end
   end
 end
