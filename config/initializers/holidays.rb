ff4803ec41fc4aa70adc38d00819fb0a74255baf
Paul Battley <pbattley@gmail.com>
2015-11-25 09:25:14 +0000

    fixup! Fetch a list of public holidays from gov.uk

--- config/initializers/holidays.rb
+++ config/initializers/holidays.rb
@@ -1,51 +1,26 @@
 Rails.application.config.holidays = [
-  Date.new(2012, 1, 2),
-  Date.new(2012, 4, 6),
-  Date.new(2012, 4, 9),
-  Date.new(2012, 5, 7),
-  Date.new(2012, 6, 4),
-  Date.new(2012, 6, 5),
-  Date.new(2012, 8, 27),
-  Date.new(2012, 12, 25),
-  Date.new(2012, 12, 26),
-  Date.new(2013, 1, 1),
-  Date.new(2013, 3, 29),
-  Date.new(2013, 4, 1),
-  Date.new(2013, 5, 6),
-  Date.new(2013, 5, 27),
-  Date.new(2013, 8, 26),
-  Date.new(2013, 12, 25),
-  Date.new(2013, 12, 26),
-  Date.new(2014, 1, 1),
-  Date.new(2014, 4, 18),
-  Date.new(2014, 4, 21),
-  Date.new(2014, 5, 5),
-  Date.new(2014, 5, 26),
-  Date.new(2014, 8, 25),
-  Date.new(2014, 12, 25),
-  Date.new(2014, 12, 26),
-  Date.new(2015, 1, 1),
-  Date.new(2015, 4, 3),
-  Date.new(2015, 4, 6),
-  Date.new(2015, 5, 4),
-  Date.new(2015, 5, 25),
-  Date.new(2015, 8, 31),
-  Date.new(2015, 12, 25),
-  Date.new(2015, 12, 28),
-  Date.new(2016, 1, 1),
-  Date.new(2016, 3, 25),
-  Date.new(2016, 3, 28),
-  Date.new(2016, 5, 2),
-  Date.new(2016, 5, 30),
-  Date.new(2016, 8, 29),
-  Date.new(2016, 12, 26),
-  Date.new(2016, 12, 27),
-  Date.new(2017, 1, 2),
-  Date.new(2017, 4, 14),
-  Date.new(2017, 4, 17),
-  Date.new(2017, 5, 1),
-  Date.new(2017, 5, 29),
-  Date.new(2017, 8, 28),
-  Date.new(2017, 12, 25),
-  Date.new(2017, 12, 26)
+  Date.new(2015,  1,  1), # New Year’s Day
+  Date.new(2015,  4,  3), # Good Friday
+  Date.new(2015,  4,  6), # Easter Monday
+  Date.new(2015,  5,  4), # Early May bank holiday
+  Date.new(2015,  5, 25), # Spring bank holiday
+  Date.new(2015,  8, 31), # Summer bank holiday
+  Date.new(2015, 12, 25), # Christmas Day
+  Date.new(2015, 12, 28), # Boxing Day
+  Date.new(2016,  1,  1), # New Year’s Day
+  Date.new(2016,  3, 25), # Good Friday
+  Date.new(2016,  3, 28), # Easter Monday
+  Date.new(2016,  5,  2), # Early May bank holiday
+  Date.new(2016,  5, 30), # Spring bank holiday
+  Date.new(2016,  8, 29), # Summer bank holiday
+  Date.new(2016, 12, 26), # Boxing Day
+  Date.new(2016, 12, 27), # Christmas Day
+  Date.new(2017,  1,  2), # New Year’s Day
+  Date.new(2017,  4, 14), # Good Friday
+  Date.new(2017,  4, 17), # Easter Monday
+  Date.new(2017,  5,  1), # Early May bank holiday
+  Date.new(2017,  5, 29), # Spring bank holiday
+  Date.new(2017,  8, 28), # Summer bank holiday
+  Date.new(2017, 12, 25), # Christmas Day
+  Date.new(2017, 12, 26)  # Boxing Day
 ]
--- lib/tasks/public_holidays.rake
+++ lib/tasks/public_holidays.rake
@@ -1,13 +1,24 @@
 require 'open-uri'
 
-task :update_holidays do
+task :update_holidays => :environment do
   hash = JSON.parse(open('https://www.gov.uk/bank-holidays.json').read)
+
+  events = hash.
+    fetch('england-and-wales').
+    fetch('events').
+    map { |event| [Date.parse(event.fetch('date')), event.fetch('title')] }.
+    select { |date, _| date.year >= Time.zone.today.year }
+
   File.open('config/initializers/holidays.rb', 'w') do |f|
     f.puts 'Rails.application.config.holidays = ['
-    f.puts hash.fetch('england-and-wales').fetch('events').map { |event|
-      date = Date.parse(event.fetch('date'))
-      "  Date.new(#{date.year}, #{date.month}, #{date.mday})"
-    }.join(",\n")
+
+    while events.any?
+      date, title = events.shift
+      f.print '  Date.new(%4d, %2d, %2d)' % [date.year, date.month, date.mday]
+      f.print events.any? ? ',' : ' '
+      f.puts " # #{title}"
+    end
+
     f.puts ']'
   end
 end
