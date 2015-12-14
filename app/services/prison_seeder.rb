8295a4c51dca4bb9c359d1f20690b2c22dfa9d94
Paul Battley <pbattley@gmail.com>
2015-12-14 17:00:49 +0000

    Make seeding a bit more formal
    There is now a PrisonSeeder class with tests of the hairier parts.

---
+++ app/services/prison_seeder.rb
@@ -0,0 +1,114 @@
+class PrisonSeeder
+  def self.seed!(base_path)
+    filename_to_uuid_map_path =
+      base_path.join('prison_uuid_mappings.yml')
+    filename_to_uuid_map =
+      YAML.load(File.read(filename_to_uuid_map_path)) || {}
+    seeder = new(filename_to_uuid_map)
+
+    Dir[base_path.join('prisons', '*.yml')].each do |path|
+      seeder.import path, YAML.load(File.read(path))
+    end
+  end
+
+  def initialize(filename_to_uuid_map)
+    @filename_to_uuid_map = filename_to_uuid_map
+  end
+
+  def import(path, hash)
+    prison = Prison.find_or_initialize_by(id: uuid_for_path(path))
+    entry = SeedEntry.new(hash)
+    prison.update! entry.to_h
+  end
+
+private
+
+  def uuid_for_path(path)
+    filename = File.basename(path)
+
+    unless @filename_to_uuid_map.key?(filename)
+      fail Prison::MissingUuidMapping, <<-EOF.strip_heredoc
+        #{filename} is missing a UUID mapping.
+        Rerun `rake maintenance:prison_uuids`, commit the result and rerun
+        `rake db:seed`.
+      EOF
+    end
+
+    @filename_to_uuid_map.fetch(filename)
+  end
+
+  class SeedEntry
+    DEFAULT_BOOKING_WINDOW = 28
+    DEFAULT_LEAD_DAYS = 3
+    DEFAULT_ADULT_AGE = 18
+
+    KEYS = %i[
+      address adult_age booking_window email_address enabled estate lead_days
+      name nomis_id phone_no slot_details weekend_processing
+    ]
+
+    def initialize(hash)
+      @hash = hash
+    end
+
+    def to_h
+      KEYS.inject({}) { |a, e| a.merge(e => send(e)) }
+    end
+
+  private
+
+    attr_reader :hash
+
+    def address
+      hash.fetch('address', []).join("\n")
+    end
+
+    def adult_age
+      hash.fetch('adult_age', DEFAULT_ADULT_AGE)
+    end
+
+    def booking_window
+      hash.fetch('booking_window', DEFAULT_BOOKING_WINDOW)
+    end
+
+    def email_address
+      hash.fetch('email', nil)
+    end
+
+    def enabled
+      hash.fetch('enabled', true)
+    end
+
+    def estate
+      hash.fetch('estate')
+    end
+
+    def lead_days
+      hash.fetch('lead_days', DEFAULT_LEAD_DAYS)
+    end
+
+    def name
+      hash.fetch('name')
+    end
+
+    def nomis_id
+      hash.fetch('nomis_id')
+    end
+
+    def phone_no
+      hash.fetch('phone', nil)
+    end
+
+    def slot_details
+      {
+        'recurring' => hash.fetch('slots', {}),
+        'anomalous' => hash.fetch('slot_anomalies', {}),
+        'unbookable' => hash.fetch('unbookable', [])
+      }
+    end
+
+    def weekend_processing
+      hash.fetch('works_weekends', false)
+    end
+  end
+end
---
+++ spec/services/prison_seeder_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+RSpec.describe PrisonSeeder do
+  context 'importing from disk' do
+    let(:base_path) { Rails.root.join('spec', 'fixtures', 'seeds') }
+
+    it 'imports data from the YAML files' do
+      expect {
+        described_class.seed!(base_path)
+      }.to change(Prison, :count).by(2)
+    end
+
+    it 'imports using the UUID mapping' do
+      described_class.seed!(base_path)
+      expect(Prison.find('67d22c66-41ac-431b-b24a-51bafb30ef8d')).
+        to have_attributes(name: 'Lunar Penal Colony')
+    end
+  end
+
+  context 'importing when the UUID is not mapped' do
+    let(:filename_to_uuid_map) { {} }
+    subject { described_class.new(filename_to_uuid_map) }
+
+    it 'raises an exception on import' do
+      expect {
+        subject.import 'LNX-luna.yml', {}
+      }.to raise_exception(Prison::MissingUuidMapping)
+    end
+  end
+
+  context 'import' do
+    subject { described_class.new(filename_to_uuid_map) }
+    let(:filename) { 'LNX-luna.yml' }
+    let(:uuid) { '0ff01907-42f6-4646-9bda-841ec27d4fc6' }
+    let(:filename_to_uuid_map) { { filename => uuid } }
+    let(:hash) {
+      {
+        'name' => 'Lunar Penal Colony',
+        'nomis_id' => 'LNX',
+        'address' => ['Outer Rim', 'Eratosthenes', 'Mare Imbrium', 'Luna'],
+        'booking_window' => 28,
+        'email' => 'luna@hmps.gsi.gov.uk',
+        'enabled' => true,
+        'estate' => 'Luna',
+        'phone' => '0115 4960123',
+        'slot_anomalies' => {
+          Date.new(2015, 5, 25) => ['1330-1430'],
+          Date.new(2015, 8, 31) => ['1330-1430']
+        },
+        'slots' => {
+          'mon' => ['1330-1430'],
+          'tue' => ['1330-1430']
+        },
+        'unbookable' => [Date.new(2015, 11, 4)]
+      }
+    }
+
+    it 'creates a new record' do
+      expect {
+        subject.import filename, hash
+      }.to change(Prison, :count).by(1)
+    end
+
+    it 'updates an existing record' do
+      create :prison, id: uuid
+      expect {
+        subject.import filename, hash
+      }.not_to change(Prison, :count)
+      expect(Prison.find(uuid).name).to eq('Lunar Penal Colony')
+    end
+
+    it 'transforms slot details' do
+      subject.import filename, hash
+      expect(Prison.find(uuid).slot_details).to eq(
+        "recurring" => {
+          "mon" => ["1330-1430"],
+          "tue" => ["1330-1430"]
+        },
+        "anomalous" => {
+          "2015-05-25" => ["1330-1430"],
+          "2015-08-31" => ["1330-1430"]
+        },
+        "unbookable" => ["2015-11-04"]
+      )
+    end
+  end
+end
