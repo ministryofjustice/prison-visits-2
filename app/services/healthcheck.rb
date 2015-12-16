337c5c130a913f0a67302c218f6ee10c6790edd8
Paul Battley <pbattley@gmail.com>
2015-12-16 09:19:05 +0000

    Add SMTP status to healthcheck

--- app/services/healthcheck.rb
+++ app/services/healthcheck.rb
@@ -5,7 +5,8 @@ class Healthcheck
     @components = {
       database: DatabaseCheck.new('Postgres database'),
       mailers: QueueCheck.new('Email queue', queue_name: 'mailers'),
-      zendesk: QueueCheck.new('Zendesk queue', queue_name: 'zendesk')
+      zendesk: QueueCheck.new('Zendesk queue', queue_name: 'zendesk'),
+      smtp: SmtpCheck.new('SMTP server', smtp_settings: smtp_settings)
     }
   end
 
@@ -17,5 +18,9 @@ class Healthcheck
     @components.inject(ok: ok?) { |hash, (key, checker)|
       hash.merge(key => checker.report)
     }
+  end
+
+  def smtp_settings
+    Rails.configuration.action_mailer.smtp_settings
   end
 end
