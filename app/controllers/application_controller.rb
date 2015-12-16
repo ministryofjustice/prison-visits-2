48e81f05fc0428ae6c071727addbedca70e48d0c
Paul Battley <pbattley@gmail.com>
2015-12-16 11:45:36 +0000

    Inhibit caching in browsers and proxies
    We don't want intermediates to cache booking requests, and we don't want
    other users of the same computer to be able to see what was previously
    booked.
    
    See https://stackoverflow.com/questions/49547 for more detail about the
    headers used.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :do_not_cache
+
   helper LinksHelper
 
 private
@@ -13,5 +15,11 @@ private
 
   def http_user_agent
     request.headers['HTTP_USER_AGENT']
+  end
+
+  def do_not_cache
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
   end
 end
