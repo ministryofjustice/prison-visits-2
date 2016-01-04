d3fa95ac48ae84ed1e5818b063f7519cc4d250fe
Paul Battley <pbattley@gmail.com>
2016-01-04 14:50:14 +0000

    Raise exception if recurring slot is invalid
    At present, this will only raise at runtime, but it moves us toward
    being able to verify the prison data in advance.
    
    Using exceptions is consistent with how other data types used in slot
    configuration - i.e. Date - work. In future, if we add a front-end to
    edit this data, we may not want it to raise exceptions. But at that
    point we won't want it in the same format in any case.

--- app/models/recurring_slot.rb
+++ app/models/recurring_slot.rb
@@ -1,9 +1,28 @@
-RecurringSlot = Struct.new(
-  :begin_hour, :begin_minute, :end_hour, :end_minute
-) do
+TimeRange = Struct.new(:begin_hour, :begin_minute, :end_hour, :end_minute)
+
+class RecurringSlot < TimeRange
+  ParseError = Class.new(ArgumentError)
+  InvalidRange = Class.new(ArgumentError)
+
+  PARSE_PATTERN = /
+    \A
+    ([01][0-9] | 2[0-3]) ([0-5][0-9]) -
+    ([01][0-9] | 2[0-3]) ([0-5][0-9])
+    \z
+  /x
+
   def self.parse(text_range)
-    matches = text_range.match(/\A(\d\d)(\d\d)-(\d\d)(\d\d)\z/)
+    matches = text_range.match(PARSE_PATTERN)
+    fail ParseError, "Cannot parse '#{text_range}'" unless matches
     new(*matches[1, 4].map { |v| v.to_i(10) })
+  end
+
+  def initialize(*)
+    super
+
+    unless valid_hours? && valid_minutes? && ends_after_begins?
+      fail InvalidRange, 'Invalid range'
+    end
   end
 
   def on(date)
@@ -11,5 +30,19 @@ RecurringSlot = Struct.new(
       date.year, date.month, date.day,
       begin_hour, begin_minute, end_hour, end_minute
     )
+  end
+
+private
+
+  def valid_hours?
+    [begin_hour, end_hour].all? { |h| (0..23).include?(h) }
+  end
+
+  def valid_minutes?
+    [begin_minute, end_minute].all? { |m| (0..59).include?(m) }
+  end
+
+  def ends_after_begins?
+    (end_hour * 60 + end_minute) > (begin_hour * 60 + begin_minute)
   end
 end
---
+++ spec/models/recurring_slot_spec.rb
@@ -0,0 +1,38 @@
+RSpec.describe RecurringSlot do
+  context 'when parsed from a valid description' do
+    subject { described_class.parse('0930-1445') }
+
+    it {
+      is_expected.to have_attributes(
+        begin_hour: 9,
+        begin_minute: 30,
+        end_hour: 14,
+        end_minute: 45
+      )
+    }
+  end
+
+  context 'when parsed from an invalid description' do
+    it 'raises an exception' do
+      expect {
+        described_class.parse('THIS IS JUNK')
+      }.to raise_exception(described_class::ParseError)
+    end
+  end
+
+  context 'when parsed from impossible times' do
+    it 'raises an exception' do
+      expect {
+        described_class.parse('9900-2460')
+      }.to raise_exception(described_class::ParseError)
+    end
+  end
+
+  context 'when initialized with an end before the start' do
+    it 'raises an exception' do
+      expect {
+        described_class.new(12, 00, 11, 10)
+      }.to raise_exception(described_class::InvalidRange)
+    end
+  end
+end
