a7972334715db134b3de0e5c22aa05a48102578b
Paul Battley <pbattley@gmail.com>
2016-01-05 10:00:21 +0000

    Parse slot details eagerly
    This will catch problems with the data earlier.

--- app/services/slot_details_parser.rb
+++ app/services/slot_details_parser.rb
@@ -1,21 +1,27 @@
 class SlotDetailsParser
   def initialize(raw)
-    @raw = raw
+    @recurring_slots = parse_recurring_slots(raw)
+    @anomalous_slots = parse_anomalous_slots(raw)
+    @unbookable_dates = parse_unbookable_dates(raw)
   end
 
-  def recurring_slots
-    @raw.fetch('recurring', {}).map { |day, slots|
+  attr_reader :recurring_slots, :anomalous_slots, :unbookable_dates
+
+private
+
+  def parse_recurring_slots(raw)
+    raw.fetch('recurring', {}).map { |day, slots|
       [DayOfWeek.by_name(day), slots.map { |s| RecurringSlot.parse(s) }]
     }.to_h
   end
 
-  def anomalous_slots
-    @raw.fetch('anomalous', {}).map { |date, slots|
+  def parse_anomalous_slots(raw)
+    raw.fetch('anomalous', {}).map { |date, slots|
       [Date.parse(date), slots.map { |s| RecurringSlot.parse(s) }]
     }.to_h
   end
 
-  def unbookable_dates
-    @raw.fetch('unbookable', {}).map { |date| Date.parse(date) }
+  def parse_unbookable_dates(raw)
+    raw.fetch('unbookable', {}).map { |date| Date.parse(date) }
   end
 end
