e8257e0fa4295ab5d87ddf385eecebbe29aa499d
Martyn Loughran <me@mloughran.com>
2016-01-20 11:56:27 +0000

    Explicit check that only valid paths are rendered

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,17 @@
 class ErrorsController < ApplicationController
+  SUPPORTED_ERRORS = {
+    404 => :'404',
+    500 => :'500',
+    503 => :'503'
+  }
+
   def show
-    status_code = params.fetch(:status_code, 500)
-    render status_code.to_s, status: status_code
+    status_code = params.fetch(:status_code).to_i
+
+    # Explicity protect against rendering an unexpected template (although such
+    # a situation should not be possible with a correct routes definition)
+    template_to_render = SUPPORTED_ERRORS.fetch(status_code)
+
+    render template_to_render, status: status_code
   end
 end
