ec57c0062b18727842ca31fb6467265091b24c22
Martyn Loughran <me@mloughran.com>
2016-01-21 11:41:43 +0000

    Clean up the static page specs

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -4,12 +4,9 @@ RSpec.describe ErrorsController do
   render_views
 
   %w[ 404 500 503 ].each do |status_code|
-    describe "when rendering #{status_code} page" do
-      before do
-        get :show, status_code: status_code
-      end
-
-      specify { expect(response.status).to eq(status_code.to_i) }
+    it "renders #{status_code} page with the given status" do
+      get :show, status_code: status_code
+      expect(response.status).to eq(status_code.to_i)
     end
   end
 end
--- spec/controllers/high_voltage/pages_controller_spec.rb
+++ spec/controllers/high_voltage/pages_controller_spec.rb
@@ -4,12 +4,9 @@ RSpec.describe HighVoltage::PagesController do
   render_views
 
   %w[ cookies terms_and_conditions unsubscribe ].each do |page_name|
-    describe "when rendering #{page_name}" do
-      before do
-        get :show, id: page_name
-      end
-
-      specify { expect(response.status).to eq(200) }
+    it "renders #{page_name} successfully" do
+      get :show, id: page_name
+      expect(response).to have_http_status(:ok)
     end
   end
 end
