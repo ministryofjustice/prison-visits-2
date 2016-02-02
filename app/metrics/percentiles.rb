736a10d56f19e6f9fea4fd7e33078129e56ede71
Todd Tyree <tatyree@gmail.com>
2016-02-02 16:25:41 +0000

    Refactor centiles to work with explicit data inputs
    Despite receiving objects, the formatter is sensitive to the ordering of
    the attributes.  That is a difficult challenge given the nature of the
    metrics logic.  Hopefully, the inclusion of the `ordered_counters`
    method will make everything clearer.

--- app/metrics/percentiles.rb
+++ app/metrics/percentiles.rb
@@ -10,13 +10,22 @@ module Percentiles
 
   class DistributionByPrison < ActiveRecord::Base
     extend PercentileSupport
+    def self.ordered_counters
+      pluck(:prison_name, :percentiles)
+    end
   end
 
   class DistributionByPrisonAndCalendarWeek < ActiveRecord::Base
     extend PercentileSupport
+    def self.ordered_counters
+      pluck(:prison_name, :year, :week, :percentiles)
+    end
   end
 
   class DistributionByPrisonAndCalendarDate < ActiveRecord::Base
     extend PercentileSupport
+    def self.ordered_counters
+      pluck(:prison_name, :year, :month, :day, :percentiles)
+    end
   end
 end
--- app/metrics/support/percentile_support.rb
+++ app/metrics/support/percentile_support.rb
@@ -10,13 +10,12 @@ module PercentileSupport
   end
 
   def fetch_and_format(aggregate = nil)
-    all.each_with_object({}) do |distrib, result|
-      metrics = distrib.attributes
-      metrics['percentiles'] = hash_centiles(metrics.delete('percentiles'))
-      prison = metrics.delete('prison_name')
+    ordered_counters.each_with_object({}) do |values, result|
+      prison = values.shift
+      values << hash_centiles(values.pop)
       result_key = aggregate.blank? ? prison : 'all'
       result[result_key] = result.fetch(result_key, {}).deep_merge(
-        order_and_hash_visit_values(metrics)
+        order_and_hash_visit_values(values)
       )
     end
   end
