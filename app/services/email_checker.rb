c62dfe356a3bfe42da0acaa82ec3e5edf725a505
Martyn Loughran <me@mloughran.com>
2016-02-11 12:19:50 +0000

    Refactor EmailChecker
    The overuse of delegation was making the code opaque.
    
    In particular it’s rather useful to know where Sendgrid API calls are being make, and this was previously hidden

--- app/services/email_checker.rb
+++ app/services/email_checker.rb
@@ -1,8 +1,4 @@
 class EmailChecker
-  extend Forwardable
-  def_delegators :SendgridApi, :bounced?, :spam_reported?
-  def_delegator :error, :valid?
-
   def initialize(original_address, override_sendgrid = false)
     @original_address = original_address
     @parsed = parse_address(original_address)
@@ -17,18 +13,22 @@ class EmailChecker
     I18n.t(error, scope: 'email_checker.errors')
   end
 
+  def valid?
+    error.valid?
+  end
+
   def delivery_error_occurred?
     error.spam_reported? || error.bounced?
   end
 
   def reset_bounce?
     return false unless parsed
-    override_sendgrid? && bounced?(parsed.address)
+    override_sendgrid? && SendgridApi.bounced?(parsed.address)
   end
 
   def reset_spam_report?
     return false unless parsed
-    override_sendgrid? && spam_reported?(parsed.address)
+    override_sendgrid? && SendgridApi.spam_reported?(parsed.address)
   end
 
 private
@@ -37,14 +37,15 @@ private
 
   # rubocop:disable Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/PerceivedComplexity
+  # rubocop:disable Metrics/MethodLength
   def compute_error
     return :unparseable unless parsed
     return :domain_dot if domain_dot_error?
     return :malformed unless well_formed_address?
     return :no_mx_record unless mx_records?
     unless override_sendgrid?
-      return :spam_reported if spam_reported?(parsed.address)
-      return :bounced if bounced?(parsed.address)
+      return :spam_reported if SendgridApi.spam_reported?(parsed.address)
+      return :bounced if SendgridApi.bounced?(parsed.address)
     end
     :valid
   end
