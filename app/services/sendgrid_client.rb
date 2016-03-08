653c442673f3be8d46aef1876615208ac909634e
Alan Kennedy <alankennedy100@gmail.com>
2016-03-08 11:45:41 +0000

    Refactor of SendgridClient
    GET and POST both use query params so the logic can be reused in the private
    method to build the options.

--- app/services/sendgrid_client.rb
+++ app/services/sendgrid_client.rb
@@ -20,31 +20,24 @@ class SendgridClient
   def get_request(endpoint, query, timeout: TIMEOUT)
     fail Error, 'no credentials set' if credentials_missing?
 
-    options = base_opts(timeout).merge(
-      path: "api/#{endpoint}",
-      query: query.merge(credentials)
-    )
-
-    JSON.parse(@connection.get(options).body)
+    JSON.parse(@connection.get(options(endpoint, query, timeout)).body)
   end
 
   def post_request(endpoint, data, timeout: TIMEOUT)
     fail Error, 'no credentials set' if credentials_missing?
 
-    options = base_opts(timeout).merge(
-      path: "api/#{endpoint}",
-      query: data.merge(credentials)
-    )
-
-    JSON.parse(@connection.post(options).body)
+    JSON.parse(@connection.post(options(endpoint, data, timeout)).body)
   end
 
-  def base_opts(timeout)
+  def options(endpoint, query, timeout)
     {
       expects: [200],
       headers: {
         'Accept' => 'application/json'
       },
+      path: "api/#{endpoint}",
+      # Sendgrid only allows query string for both Post and Get
+      query: query.merge(credentials),
       read_timeout: timeout,
       write_timeout: timeout
     }
