f50e721f1cbd796be802f30db9d09a00c7416aac
Martyn Loughran <me@mloughran.com>
2016-04-08 10:51:47 +0100

    Optionally allow fetching nomis slots via slots API

--- app/controllers/api/slots_controller.rb
+++ app/controllers/api/slots_controller.rb
@@ -1,9 +1,18 @@
 module Api
   class SlotsController < ApiController
+    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Metrics/MethodLength
     def index
       prison = Prison.enabled.find(params.fetch(:prison_id))
 
-      slot_availability = SlotAvailability.new(prison: prison)
+      # Temporarily introduce this API parameter to facilitate experimentation
+      # (Note: using string 'true' because this is a GET parameter)
+      use_nomis = params.fetch(:use_nomis_slots, nil) == 'true'
+
+      slot_availability = SlotAvailability.new(
+        prison: prison,
+        use_nomis_slots: use_nomis
+      )
       slot_availability.restrict_by_prisoner(
         prisoner_number: params.fetch(:prisoner_number),
         prisoner_dob: Date.parse(params.fetch(:prisoner_dob))
@@ -11,5 +20,7 @@ module Api
 
       @slots = slot_availability.slots
     end
+    # rubocop:enable Metrics/AbcSize
+    # rubocop:enable Metrics/MethodLength
   end
 end
