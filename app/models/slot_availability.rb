24a5446596de02e3a460559bc757f01a26092a1d
Martyn Loughran <me@mloughran.com>
2016-04-11 10:39:43 +0100

    Refactored slot lookup into 2 methods

--- app/models/slot_availability.rb
+++ app/models/slot_availability.rb
@@ -3,28 +3,8 @@ class SlotAvailability
 
   def initialize(prison:, use_nomis_slots: false)
     @prison = prison
-    @slots = lookup_slots_for_prison(prison, use_nomis_slots)
+    @slots = (use_nomis_slots && nomis_slots(prison)) || hardcoded_slots(prison)
   end
-
-  # rubocop:disable Metrics/MethodLength
-  def lookup_slots_for_prison(prison, use_nomis)
-    # Default to hard-coded slots if API is unavailable or returns an error
-    if use_nomis && Nomis::Api.enabled?
-      begin
-        Nomis::Api.instance.fetch_bookable_slots(
-          prison: prison,
-          start_date: prison.first_bookable_date,
-          end_date: prison.last_bookable_date
-        )
-      rescue Excon::Errors::Error => e
-        Rails.logger.warn "Error calling the NOMIS API: #{e.inspect}"
-        prison.available_slots.to_a
-      end
-    else
-      prison.available_slots.to_a
-    end
-  end
-  # rubocop:enable Metrics/MethodLength
 
   # rubocop:disable Metrics/MethodLength
   # rubocop:disable Metrics/AbcSize
@@ -51,6 +31,25 @@ class SlotAvailability
     # Skip restriction if NOMIS API is misbehaving
     Rails.logger.warn "Error calling the NOMIS API: #{e.inspect}"
   end
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/AbcSize
+
+private
+
+  def hardcoded_slots(prison)
+    prison.available_slots.to_a
+  end
+
+  def nomis_slots(prison)
+    return nil unless Nomis::Api.enabled?
+
+    Nomis::Api.instance.fetch_bookable_slots(
+      prison: prison,
+      start_date: prison.first_bookable_date,
+      end_date: prison.last_bookable_date
+    )
+  rescue Excon::Errors::Error => e
+    Rails.logger.warn "Error calling the NOMIS API: #{e.inspect}"
+    nil
+  end
 end
