f0005154d19c30a9b84ef6450cff6b2ee691dff3
Martyn Loughran <me@mloughran.com>
2016-04-15 16:34:32 +0100

    Gracefully handle invalid dates in validation API

--- app/controllers/api/validations_controller.rb
+++ app/controllers/api/validations_controller.rb
@@ -1,9 +1,16 @@
 module Api
   class ValidationsController < ApiController
+    # rubocop:disable Metrics/MethodLength
     def prisoner
+      begin
+        date = Date.parse(params.fetch(:date_of_birth))
+      rescue ArgumentError
+        raise ParameterError, 'date_of_birth'
+      end
+
       validation = PrisonerValidation.new(
         noms_id: params.fetch(:number),
-        date_of_birth: Date.parse(params.fetch(:date_of_birth))
+        date_of_birth: date
       )
 
       if validation.valid?
@@ -14,5 +21,6 @@ module Api
 
       render status: 200, json: { validation: response }
     end
+    # rubocop:enable Metrics/MethodLength
   end
 end
