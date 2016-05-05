f2bbcc54440693dd4fee4a3e3f2c498a37bd5458
Alan Kennedy <alankennedy100@gmail.com>
2016-05-05 13:43:35 +0100

    Set the puma threads max to be the db pool size
    Fixes an issue where Puma can accept a request but error out because the DB pool
    times out. This happened a couple of months ago when the Sendgrid API started
    becoming unresponsive and we didn't have a pool of Sendgrid clients.
    
    The fix prevents the puma and DB pools be out of sync by using the same source
    for the max number of threads
    
    This is similar to how Rails 5 will be configured by default with Puma [1].
    Although with Rails 5 Puma can be configured via the command line with `rails s`
    so we can revisit this when we upgrade.
    
    [1] https://blog.heroku.com/archives/2016/5/2/container_ready_rails_5

--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ migrate_and_seed)
     bundle exec rake db:migrate db:seed
     ;;
 esac
-bundle exec rails server -d --binding 0.0.0.0
+bundle exec puma -b tcp://0.0.0.0:3000 -d -C config/puma_prod.rb
 tail -f /usr/src/app/log/logstash_production.json
