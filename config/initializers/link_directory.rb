a070821a95eeb0103cf989c4dd95c3c9b336ddf0
Alan Kennedy <alankennedy100@gmail.com>
2016-05-16 13:44:57 +0100

    Fix rubocop

--- config/initializers/link_directory.rb
+++ config/initializers/link_directory.rb
@@ -1,4 +1,8 @@
-public_service_url = Rails.env.production? ? ENV.fetch('PUBLIC_SERVICE_URL') : 'http://localhost:4000'
+public_service_url = if Rails.env.production?
+                       ENV.fetch('PUBLIC_SERVICE_URL')
+                     else
+                       'http://localhost:4000'
+                     end
 
 Rails.configuration.link_directory = LinkDirectory.new(
   prison_finder: 'http://www.justice.gov.uk/contacts/prison-finder{/prison}',
