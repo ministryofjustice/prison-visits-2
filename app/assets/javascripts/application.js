dfb324041a5adf086af1e11d2b9b44a8789d9f98
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2016-05-16 09:45:52 +0100

    Remove unused JS and CSS assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,6 @@
 // Vendor assets
-//= require handlebars
 //= require lodash
 //= require jquery-ui-autocomplete
-//= require vendor/modernizr.custom.85598
-//= require dist/javascripts/moj.slot-picker
-//= require dist/javascripts/moj.date-slider
 //= require dest/respond.min
 
 // GOVUK modules
@@ -18,6 +14,4 @@
 //= require modules/moj.hijacks
 //= require modules/moj.submit-once
 //= require modules/moj.Conditional
-//= require modules/moj.RevealAdditional
 //= require modules/moj.checkbox-summary
-//= require modules/moj.AgeLabel.js
