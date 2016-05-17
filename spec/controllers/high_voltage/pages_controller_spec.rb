c938b17a74389ae875f50daccabc96e213074204
Martyn Loughran <me@mloughran.com>
2016-05-17 10:31:11 +0100

    Remove static pages for T&Cs and Cookies
    Replaced in footer with a link to the staff guide.

--- spec/controllers/high_voltage/pages_controller_spec.rb
+++ spec/controllers/high_voltage/pages_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe HighVoltage::PagesController do
   render_views
 
-  %w[ cookies terms_and_conditions unsubscribe ].each do |page_name|
+  %w[ unsubscribe ].each do |page_name|
     it "renders #{page_name} successfully" do
       get :show, id: page_name
       expect(response).to have_http_status(:ok)
