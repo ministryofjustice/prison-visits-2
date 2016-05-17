63ab65d2d4e69cc01311cfe9a61d09f36bae7dae
Alan Kennedy <alankennedy100@gmail.com>
2016-05-17 11:37:42 +0100

    Load requested visits per estate

--- app/controllers/prison/dashboards_controller.rb
+++ app/controllers/prison/dashboards_controller.rb
@@ -6,6 +6,13 @@ class Prison::DashboardsController < ApplicationController
   end
 
   def show
-    @estate = Estate.find_by(finder_slug: params[:estate_id])
+    @estate = Estate.find_by!(finder_slug: params[:estate_id])
+
+    @requested_visits = Visit.
+                        includes(:prisoner, :visitors).
+                        with_processing_state(:requested).
+                        from_estate(@estate).
+                        order('created_at asc').
+                        all
   end
 end
