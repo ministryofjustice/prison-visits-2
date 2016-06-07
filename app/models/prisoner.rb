263ea1539af66ebe917ea68ec07de84170eebd97
Alan Kennedy <alankennedy100@gmail.com>
2016-06-07 11:50:13 +0100

    Upcase prisoner number for new visits
    First step to have consistent prisoner numbers.

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -4,4 +4,8 @@ class Prisoner < ActiveRecord::Base
 
   has_many :visits, dependent: :destroy
   validates :number, presence: true
+
+  before_validation(on: :create) do
+    self.number = number.upcase.strip if attribute_present?('number')
+  end
 end
