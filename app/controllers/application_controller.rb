a9975dba8484436a8a36bb2460ab1711bdcc87eb
Alan Kennedy <alankennedy100@gmail.com>
2016-06-13 11:00:38 +0100

    Redirect to page after sign in
    As per devise documentation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :do_not_cache
   before_action :set_locale
   before_action :store_request_id
+  before_action :store_current_location, unless: :devise_controller?
 
   helper LinksHelper
 
@@ -63,7 +64,14 @@ private
     Raven.extra_context(request_id: RequestStore.store[:request_id])
   end
 
-  def after_sign_in_path_for(_resource)
-    prison_unprocessed_visits_path
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || prison_unprocessed_visits_path
+  end
+
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
   end
 end
