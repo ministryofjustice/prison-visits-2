92753103e42729a157e900906d068447ecab44a2
Alan Kennedy <alankennedy100@gmail.com>
2016-07-12 15:42:51 +0100

    Send sidekiq logstash logs to Docker
    Tail the logstash file on the Docker Sidekiq script so that they get sent to
    logstash.

--- run-sidekiq.sh
+++ run-sidekiq.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export RAILS_ENV=production
 bundle exec rackup sidekiq-admin.ru -p 3000 -E production -o 0.0.0.0 &
-bundle exec sidekiq -l /var/log/sidekiq.log --environment production
+bundle exec sidekiq --daemon -l /var/log/sidekiq.log --environment production
+
+tail -f /usr/src/app/log/logstash_production.json
