c0b516e54123efd37807b598711df5933f324a38
Alan Kennedy <alankennedy100@gmail.com>
2016-07-13 09:04:40 +0100

    Create PVB accounts for new users
    Before this change only users with an existing account could sign in.
    
    Now when a new user signs in we create a new PVB user if they have permissions
    to an estate.
    
    There is a few things to do following this:
      - Check the permissions of existing users
      - Assumes that users only have access to one estate when creating the user
      - Assign values to Estate#sso_organisation_name and update the importer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,10 @@
 class SessionsController < ApplicationController
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/MethodLength
   def create
     auth_hash = request.env['omniauth.auth']
-    email = auth_hash.fetch('info').fetch('email')
 
     # For not only allow authorize exisiting users, since we cannot yet
     # determine which estate a new user should be assigned to
-    user = User.find_by(email: email)
+    user = User.from_sso(auth_hash.fetch('info'))
 
     if user
       session[:current_user_id] = user.id
@@ -19,8 +16,6 @@ class SessionsController < ApplicationController
       redirect_to root_path
     end
   end
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/MethodLength
 
   def destroy
     session.delete(:current_user_id)
