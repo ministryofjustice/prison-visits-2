be87094854232a0b5d00e39a1b35f7fdd6a29891
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2016-10-10 15:46:49 +0100

    API includes rejection reasons

--- app/models/rejection.rb
+++ app/models/rejection.rb
@@ -15,10 +15,8 @@ class Rejection < ActiveRecord::Base
   ].freeze
 
   belongs_to :visit
-  before_validation :copy_reason_to_reasons
-  validates :reason, inclusion: { in: REASONS }
-  validate :validate_reasons
 
+  validates :reason, inclusion: { in: REASONS }
   validate :validate_reasons
 
   def privileged_allowance_available?
