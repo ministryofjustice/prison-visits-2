594797eae6b539c8af2b871cdf89c82230ce7316
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2016-10-10 15:46:49 +0100

    API includes rejection reasons

--- spec/features/metrics_spec.rb
+++ spec/features/metrics_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'rails_helper'
 require_relative '../metrics/shared_examples_for_metrics'
 
@@ -62,6 +63,7 @@ RSpec.feature 'Metrics', js: true do
         expect(page).to have_selector('.luna-total', text: 10)
         expect(page).to have_selector('.luna-rejected', text: 5)
         expect(page).to have_selector('.luna-total-rejected', text: 50.00)
+        save_and_open_page
         expect(page).to have_selector('.luna-no-allowance', text: 20.00)
         expect(page).to have_selector('.luna-visitor-banned', text: 10.00)
         expect(page).to have_selector('.luna-slot-unavailable', text: 10.00)
