312756e724caaddee43cc0140a294c976128607d
Alan Kennedy <alankennedy100@gmail.com>
2016-10-11 13:42:11 +0100

    Add null constraint to user_id on messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927111445) do
+ActiveRecord::Schema.define(version: 20161011123918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20160927111445) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "body",                  null: false
-    t.uuid     "user_id"
+    t.uuid     "user_id",               null: false
     t.uuid     "visit_id",              null: false
     t.uuid     "visit_state_change_id"
     t.datetime "created_at",            null: false
