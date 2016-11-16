a48f5d2af07df29bdcfcc9282998892d8360f28a
Alan Kennedy <alankennedy100@gmail.com>
2016-11-16 11:47:59 +0000

    Validate requested dates for prisoner availability

--- app/models/prisoner_availability_validation.rb
+++ app/models/prisoner_availability_validation.rb
@@ -1,7 +1,7 @@
 class PrisonerAvailabilityValidation
   include NonPersistedModel
 
-  PRISONER_AVAILABILITY_UNKNOWN = 'prisoner_availibility_unknown'.freeze
+  PRISONER_AVAILABILITY_UNKNOWN = 'prisoner_availability_unknown'.freeze
   PRISONER_NOT_AVAILABLE = 'prisoner_not_available'.freeze
 
   attribute :offender, Nomis::Offender
@@ -15,15 +15,17 @@ class PrisonerAvailabilityValidation
 
 private
 
+  attr_reader :api_error
+
   def slots_availability
-    requested_dates.each do |requested_date|
+    valid_requested_dates.each do |requested_date|
       error_message = error_message_for_slot(requested_date)
       errors[requested_date.to_s] << error_message if error_message
     end
   end
 
   def error_message_for_slot(date)
-    unless Nomis::Api.enabled? && offender_availability
+    unless Nomis::Api.enabled? && offender_availability && !api_error
       return PRISONER_AVAILABILITY_UNKNOWN
     end
 
@@ -32,21 +34,29 @@ private
 
   def offender_availability
     return nil unless offender.valid?
+    # Don't want to show prisoner unavailable errors for invalid dates
+    return requested_dates unless valid_requested_dates.any?
 
     @offender_availability ||= load_offender_availability
   end
 
   def load_offender_availability
-    return nil if @prisoner_availability_error
+    return nil if @api_error
 
     Nomis::Api.instance.offender_visiting_availability(
       offender_id: offender.id,
-      start_date:  requested_dates.min,
-      end_date:    requested_dates.max
+      start_date:  valid_requested_dates.min,
+      end_date:    valid_requested_dates.max
     ).dates
-  rescue Nomis::NotFound => e
-    @prisoner_availability_error = true
+  rescue Nomis::APIError => e
+    @api_error = true
     Rails.logger.warn "Error calling the NOMIS API: #{e.inspect}"
     nil
+  end
+
+  def valid_requested_dates
+    @valid_dates ||= requested_dates.select { |date|
+      date >= Date.current && date <= 60.days.from_now.to_date
+    }
   end
 end
--- spec/models/prisoner_availability_validation_spec.rb
+++ spec/models/prisoner_availability_validation_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'nomis/client'
 
 RSpec.describe PrisonerAvailabilityValidation, type: :model do
   subject do
@@ -7,9 +8,9 @@ RSpec.describe PrisonerAvailabilityValidation, type: :model do
   end
 
   let(:offender) { Nomis::Offender.new(id: '123') }
-  let(:date1) { Date.parse('2016-10-23') }
-  let(:date2) { Date.parse('2016-10-22') }
-  let(:date3) { Date.parse('2016-10-21') }
+  let(:date1) { 2.days.from_now.to_date }
+  let(:date2) { 1.day.from_now.to_date }
+  let(:date3) { Date.current }
   let(:requested_dates) { [date1, date2, date3] }
 
   describe 'when the NOMIS API is disabled' do
@@ -28,7 +29,23 @@ RSpec.describe PrisonerAvailabilityValidation, type: :model do
   end
 
   describe 'when the NOMIS API is enabled' do
-    context 'and working correctly' do
+    context 'and the api returns an error' do
+      before do
+        expect_any_instance_of(Nomis::Client).
+          to receive(:get).and_raise(Nomis::APIError)
+      end
+
+      it 'adds an unknown error for each date' do
+        is_expected.to_not be_valid
+
+        requested_dates.each do |date|
+          expect(subject.errors[date.to_s]).
+            to eq([described_class::PRISONER_AVAILABILITY_UNKNOWN])
+        end
+      end
+    end
+
+    context 'and working correctly with valid dates' do
       before do
         allow(Nomis::Api).to receive(:enabled?).and_return(true)
 
@@ -73,19 +90,52 @@ RSpec.describe PrisonerAvailabilityValidation, type: :model do
       end
     end
 
-    context 'and the API raises an error' do
+    context 'and API enabled with invalid dates' do
       before do
-        allow_any_instance_of(Nomis::Api).
-          to receive(:offender_visiting_availability).
-          and_raise(Nomis::NotFound)
+        allow(Nomis::Api).to receive(:enabled?).and_return(true)
       end
 
-      it 'adds unknown errors to all the dates' do
-        subject.valid?
+      context 'with all the dates in the past' do
+        let(:date1) { 1.day.ago.to_date }
+        let(:date2) { 2.days.ago.to_date }
+        let(:date3) { 3.days.ago.to_date }
 
-        requested_dates.each do |date|
-          expect(subject.errors[date.to_s]).
-            to eq([described_class::PRISONER_AVAILABILITY_UNKNOWN])
+        # We return the dates as valid because it doesn't make sense to
+        # communicate that the prisoner is unavailable just because the date is
+        # in the past. Another validator will be responsible for that.
+        it 'returns all the dates' do
+          expect_any_instance_of(Nomis::Api).
+            to_not receive(:offender_visiting_availability)
+
+          subject.valid?
+
+          requested_dates.each do |date|
+            expect(subject.date_error(date)).to be_nil
+          end
+        end
+      end
+
+      context 'with some dates in the past' do
+        let(:date1) { 1.day.ago.to_date }
+        let(:date2) { 61.days.from_now.to_date }
+
+        before do
+        end
+
+        it 'filters out invalid dates' do
+          expect_any_instance_of(Nomis::Api).
+            to receive(:offender_visiting_availability).
+            with(offender_id: offender.id,
+                 start_date: date3,
+                 end_date: date3).
+            and_return(Nomis::PrisonerAvailability.new(dates: []))
+
+          subject.valid?
+
+          expect(subject.date_error(date1)).to be_nil
+          expect(subject.date_error(date2)).to be_nil
+          expect(subject.date_error(date3)).
+            to eq(described_class::PRISONER_NOT_AVAILABLE)
         end
       end
     end
