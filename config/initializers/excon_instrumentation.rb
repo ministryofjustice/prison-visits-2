c16483c923b45403bf8e0a4ce6228243e3bd4eb7
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-01-09 14:35:40 +0000

    Add comments on how Excon Instrumentation works

--- config/initializers/excon_instrumentation.rb
+++ config/initializers/excon_instrumentation.rb
@@ -1,6 +1,8 @@
 require 'pvb/excon/instrument_factory'
 
 ActiveSupport::Notifications.subscribe(/excon/) do |event, start, finish, _id, payload|
+  # Excon uses ActiveSupport if available and triggers several type of events depending
+  # on each phase of the request: excon.request, excon.retry, excon.response, excon.error
   instrument = PVB::Excon::InstrumentFactory.for(event, start.to_f, finish.to_f, payload)
   instrument.process
 end
