7e27de56b6cee56b2cc50e06df880fef51f7f0b0
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-01-09 11:42:44 +0000

    remove unused code and its tests

--- app/services/instrumentation.rb
+++ app/services/instrumentation.rb
@@ -9,33 +9,9 @@ module Instrumentation
       RequestStore.store[:custom_log_items] || {}
     end
 
-    def time_and_log(message, category = nil)
-      fail 'Block required' unless block_given?
-
-      result, time_in_ms = time_action { yield }
-
-      Rails.logger.info "#{message} – %.2fms" % [time_in_ms]
-
-      if category
-        total_time = time_in_ms + custom_log_items[category].to_i
-        append_to_log(category => total_time)
-      end
-      result
-    end
-
     def incr(counter)
       old_value = custom_log_items[counter] || 0
       append_to_log(counter => old_value + 1)
-    end
-
-  private
-
-    def time_action
-      started_at = Time.now.utc
-      result = yield
-      finished_at = Time.now.utc
-      time_in_ms = (finished_at - started_at) * 1000
-      [result, time_in_ms]
     end
   end
 end
