f658245e369b622eab42c0e5262705c61cee05f4
Alan Kennedy <alankennedy100@gmail.com>
2017-01-10 09:13:43 +0000

    Fix processing time on visit show page

--- spec/decorators/visit_decorator_spec.rb
+++ spec/decorators/visit_decorator_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe VisitDecorator do
   let(:visit) { create(:visit) }
-  subject { described_class.decorate(visit) }
+  subject(:instance) { described_class.decorate(visit) }
 
   describe '#prisoner_details_incorrect' do
     context 'when a user overrides the NOMIS validation' do
@@ -32,6 +32,30 @@ RSpec.describe VisitDecorator do
     it 'are decorated object' do
       subject.slots.each do |slot|
         expect(slot).to be_decorated
+      end
+    end
+  end
+
+  describe '#processed_at' do
+    subject(:processed_at) { instance.processed_at }
+
+    context 'when requested' do
+      let(:visit) { create(:visit, :requested) }
+
+      it 'returns the visit creation time' do
+        expect(processed_at).to eq(visit.created_at)
+      end
+    end
+
+    context 'when not requested' do
+      let!(:last_state_change) do
+        VisitStateChange.create!(visit: visit,
+                                 visit_state: 'cancelled',
+                                 created_at: 1.day.from_now)
+      end
+
+      it 'returns the last visit state change creation time' do
+        expect(processed_at).to eq(last_state_change.reload.created_at)
       end
     end
   end
