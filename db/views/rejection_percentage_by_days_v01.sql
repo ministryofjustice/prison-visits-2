0b256cbfe3882e2b30156d7edd8a1f90662a9c50
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-01-16 12:34:14 +0000

    Use CTE for big query

--- db/views/rejection_percentage_by_days_v01.sql
+++ db/views/rejection_percentage_by_days_v01.sql
@@ -4,34 +4,59 @@ WITH rejection_reasons AS (
   GROUP BY visit_id, reasons
 )
 
+
+WITH rejected_visits_count_by_prison AS (
+  SELECT prisons.name            AS prison_name,
+         prisons.id              AS prison_id,
+         visits.created_at::date AS date,
+         COUNT(*)                AS rejected_count
+  FROM visits
+  INNER JOIN prisons ON prisons.id = visits.prison_id
+  WHERE visits.processing_state    = 'rejected'
+  GROUP BY prison_name, prisons.id, date
+)
+
+WITH visit_count_by_prison AS (
+  SELECT prisons.name            AS prison_name,
+         prisons.id              AS prison_id,
+         COUNT(*)                AS total_count,
+         visits.created_at::date AS date
+  FROM visits
+  INNER JOIN prisons ON prisons.id = visits.prison_id
+  GROUP BY prison_name, prisons.id, date
+)
+
+WITH rejected_visit_count_reason_date_and_prison AS (
+  SELECT reason,
+         prisons.name            AS prison_name,
+         prisons.id              AS prison_id,
+         COUNT(*)                AS rejected_count,
+         visits.created_at::date AS date
+  FROM visits
+  INNER JOIN prisons           ON prisons.id = visits.prison_id
+  INNER JOIN rejection_reasons ON visits.id  = rejection_reasons.visit_id
+  WHERE visits.processing_state = 'rejected'
+  GROUP BY prison_name, prisons.id, reason, date
+)
+
+WITH visit_count_by_prison_and_date (
+  SELECT prisons.name            AS prison_name,
+         prisons.id              AS prison_id,
+         COUNT(*)                AS total_count,
+         visits.created_at::date AS date
+  FROM visits
+  INNER JOIN prisons ON prisons.id = visits.prison_id
+  GROUP BY prison_name, prisons.id, date
+)
+
 SELECT rejected.prison_name,
        rejected.prison_id                                     AS prison_id,
        'total'                                                AS reason,
        ROUND((rejected_count::numeric / total_count * 100),2) AS percentage,
        rejected.date                                          AS date
-FROM (
-
-       SELECT prisons.name            AS prison_name,
-              prisons.id              AS prison_id,
-              visits.created_at::date AS date,
-              COUNT(*)                AS rejected_count
-       FROM visits
-       INNER JOIN prisons ON prisons.id = visits.prison_id
-       WHERE visits.processing_state    = 'rejected'
-       GROUP BY prison_name, prisons.id, date
-
-     ) rejected,
-     (
-
-       SELECT prisons.name            AS prison_name,
-              prisons.id              AS prison_id,
-              COUNT(*)                AS total_count,
-              visits.created_at::date AS date
-       FROM visits
-       INNER JOIN prisons ON prisons.id = visits.prison_id
-       GROUP BY prison_name, prisons.id, date
-
-      ) total
+FROM
+  rejected_visits_count_by_prison AS rejected,
+  visit_count_by_prison           AS total
 WHERE rejected.prison_id = total.prison_id AND rejected.date = total.date
 GROUP BY rejected.prison_name, rejected.prison_id, reason, percentage, rejected.date
 
@@ -42,28 +67,8 @@ SELECT rejected.prison_name,
        reason,
        ROUND((rejected_count::numeric / total_count * 100),2) AS percentage,
        rejected.date                                          AS date
-FROM (
-
-        SELECT reason,
-               prisons.name            AS prison_name,
-               prisons.id              AS prison_id,
-               COUNT(*)                AS rejected_count,
-               visits.created_at::date AS date
-        FROM visits
-        INNER JOIN prisons           ON prisons.id = visits.prison_id
-        INNER JOIN rejection_reasons ON visits.id  = rejection_reasons.visit_id
-        WHERE visits.processing_state = 'rejected'
-        GROUP BY prison_name, prisons.id, reason, date
-
-     ) rejected,
-     (
-        SELECT prisons.name            AS prison_name,
-               prisons.id              AS prison_id,
-               COUNT(*)                AS total_count,
-               visits.created_at::date AS date
-        FROM visits
-        INNER JOIN prisons ON prisons.id = visits.prison_id
-        GROUP BY prison_name, prisons.id, date
-      ) total
+FROM
+  rejected_visit_count_reason_date_and_prison AS rejected,
+  visit_count_by_prison_and_date              AS  total
 WHERE rejected.prison_name = total.prison_name AND rejected.date = total.date
 GROUP BY rejected.prison_name, rejected.prison_id, reason, percentage, rejected.date
