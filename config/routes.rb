d08812fb1c26f178abc9f36814bb9fe243d53159
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2017-02-13 09:31:05 +0000

    Add jasmine-rails gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root to: 'staff_info#index'
 
   match 'exception', to: 'errors#test', via: %i[ get post ]
