55ef7cb62232174c70c5e1a47ea8535c0b52fdf9
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-03-02 14:00:49 +0000

    remove traces of instrumentation in PVB2

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -33,19 +33,9 @@ module Api
       render json: { message: message }, status: status
     end
 
-    # WARNING: This a Rails private method, could easily break in the future.
-    #
-    # Looks rather strange, but this is the suggested mechanism to add extra
-    # data into the event passed to lograge's custom options. The method is
-    # part of Rails' instrumentation code, and is run after each request.
-    def append_info_to_payload(payload)
-      super
-      payload[:custom_log_items] = Instrumentation.custom_log_items
-    end
-
     def store_request_id
       RequestStore.store[:request_id] = request.uuid
-      Instrumentation.append_to_log(request_id: RequestStore.store[:request_id])
+      PVB::Instrumentation.append_to_log(request_id: RequestStore.store[:request_id])
       Raven.extra_context(request_id: RequestStore.store[:request_id])
     end
 
--- config/initializers/pvb_instrumentation.rb
+++ config/initializers/pvb_instrumentation.rb
@@ -1,22 +1,24 @@
-PVB::Instrumentation::Registry.register(
-  "#{Nomis::Client::EXCON_INSTRUMENT_NAME}.request",
-  PVB::Instrumentation::Excon::Request
-)
+require 'pvb/instrumentation/registry'
 
-PVB::Instrumentation::Registry.register(
-  "#{Nomis::Client::EXCON_INSTRUMENT_NAME}.retry",
-  PVB::Instrumentation::Excon::Retry
-)
+PVB::Instrumentation.configure do |config|
+  config.logger = Rails.logger
+  config.register(
+    "#{Nomis::Client::EXCON_INSTRUMENT_NAME}.request",
+    PVB::Instrumentation::Excon::Request
+  )
 
-PVB::Instrumentation::Registry.register(
-  "#{Nomis::Client::EXCON_INSTRUMENT_NAME}.response",
-  PVB::Instrumentation::Excon::Response
-)
+  config.register(
+    "#{Nomis::Client::EXCON_INSTRUMENT_NAME}.retry",
+    PVB::Instrumentation::Excon::Retry)
 
-PVB::Instrumentation::Registry.register(
-  "#{Nomis::Client::EXCON_INSTRUMENT_NAME}.error",
-  PVB::Instrumentation::Excon::Error
-)
+  config.register(
+    "#{Nomis::Client::EXCON_INSTRUMENT_NAME}.response",
+    PVB::Instrumentation::Excon::Response)
 
-PVB::Instrumentation::Registry.register(
-  ''
+  config.register(
+    "#{Nomis::Client::EXCON_INSTRUMENT_NAME}.error",
+    PVB::Instrumentation::Excon::Error)
+
+  config.register(
+    'faraday.raven', PVB::Instrumentation::Faraday::Request)
+end
