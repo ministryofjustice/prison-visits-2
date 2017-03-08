e0b5e40b9140022ce9b493049db8c5238a14f4f5
Alan Kennedy <alankennedy100@gmail.com>
2017-03-08 15:25:07 +0000

    Add PrisonerDateAvailability

---
+++ app/services/nomis/prisoner_date_availability.rb
@@ -0,0 +1,35 @@
+module Nomis
+  class PrisonerDateAvailability
+    include NonPersistedModel
+
+    BOOKED_VISIT = 'booked_visit'.freeze
+    EXTERNAL_MOVEMENT = 'external_movement'.freeze
+    BANNED = 'prisoner_banned'.freeze
+    OUT_OF_VO = 'out_of_vo'.freeze
+
+    attribute :date, Date
+    attribute :banned, Boolean
+    attribute :out_of_vo, Boolean
+    attribute :external_movement, Boolean
+    attribute :existing_visits, Array
+
+    def available?(slot)
+      unavailable_reasons(slot).empty?
+    end
+
+    def unavailable_reasons(slot)
+      reasons = []
+      reasons << BANNED if banned
+      reasons << EXTERNAL_MOVEMENT if external_movement
+      reasons << OUT_OF_VO if out_of_vo
+      reasons << BOOKED_VISIT if booked_visit?(slot)
+      reasons
+    end
+
+  private
+
+    def booked_visit?(slot)
+      existing_visits.any? { |visit| visit['slot'] == slot.to_s }
+    end
+  end
+end
---
+++ spec/services/nomis/prisoner_date_availability_spec.rb
@@ -0,0 +1,100 @@
+require 'rails_helper'
+
+RSpec.describe Nomis::PrisonerDateAvailability do
+  subject(:instance) do
+    described_class.new(
+      date: date,
+      banned: banned,
+      out_of_vo: out_of_vo,
+      external_movement: external_movement,
+      existing_visits: existing_visits
+    )
+  end
+
+  let(:date) { Time.zone.today }
+  let(:banned) { false }
+  let(:out_of_vo) { false }
+  let(:external_movement) { false }
+  let(:existing_visits) { [] }
+
+  describe '#unavailable_reasons' do
+    subject { instance.unavailable_reasons(slot) }
+
+    let(:slot) do
+      ConcreteSlot.new(date.year, date.month, date.day, 14, 30, 15, 30)
+    end
+
+    context 'when banned' do
+      let(:banned) { true }
+
+      it { is_expected.to eq([described_class::BANNED]) }
+    end
+
+    context 'with no visiting allowance' do
+      let(:out_of_vo) { true }
+
+      it { is_expected.to eq([described_class::OUT_OF_VO]) }
+    end
+
+    context 'on an external movement' do
+      let(:external_movement) { true }
+
+      it { is_expected.to eq([described_class::EXTERNAL_MOVEMENT]) }
+    end
+
+    context 'with existing visits' do
+      context "different from the slot" do
+        let(:existing_visits) do
+          [
+            {
+              'slot' => ConcreteSlot.new(2017, 1, 1, 10, 30, 11, 30).to_s,
+              'id' => '123'
+            }
+          ]
+        end
+
+        it { is_expected.to be_empty }
+      end
+
+      context "same as the slot" do
+        let(:existing_visits) do
+          [
+            {
+              'slot' => slot.to_s,
+              'id' => '123'
+            }
+          ]
+        end
+
+        it { is_expected.to eq([described_class::BOOKED_VISIT]) }
+      end
+    end
+  end
+
+  describe '#available?' do
+    subject { instance.available?(slot) }
+    let(:slot) { ConcreteSlot.new(2017, 1, 1, 10, 30, 11, 30) }
+
+    context 'with no unavailable_reasons' do
+      before do
+        expect(instance).
+          to receive(:unavailable_reasons).
+          with(slot).
+          and_return([])
+      end
+
+      it { is_expected.to eq(true) }
+    end
+
+    context 'with unavailable_reasons' do
+      before do
+        expect(instance).
+          to receive(:unavailable_reasons).
+          with(slot).
+          and_return([anything])
+      end
+
+      it { is_expected.to eq(false) }
+    end
+  end
+end
