582d7a697d139e60e7c8389913343fcbd709c13b
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-04-10 10:27:40 +0100

    Add Secure Headers + some script tags hash
    Add GA Tacking
    
    Add Secure Headers
    
    Add google chart js to authorised external source
    
    Fix specs
    
    Update modernazir
    
    Update ChosenJS
    
    update pahntomjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 //= require handlebars
 //= require lodash
 //= require jquery-ui-autocomplete
-//= require vendor/modernizr.custom.85598
+//= require modernizr-custom
 //= require dest/respond.min
 //= require jquery-ui/datepicker
 
@@ -22,6 +22,7 @@
 //= require mapshim
 //= require filtershim
 //= require modules/moj.analytics
+//= require modules/moj.AsyncGA
 //= require modules/moj.autocomplete
 //= require modules/moj.clearRadioButtons
 //= require modules/moj.hijacks
