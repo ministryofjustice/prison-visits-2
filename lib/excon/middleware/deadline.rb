373820bf7dc64569a23c2fd4857d228c6b3132bb
Alan Kennedy <alankennedy100@gmail.com>
2017-04-20 09:34:43 +0100

    Deadline Excon middleware
    Introduces the concept of deadlines within Excon so that we can rely on network
    timeouts instead of the timebox timeouts.
    
    Takes a future time as an argument and sets the read / write timeouts based on
    that future time. If the time has already elapsed it raises an error.

---
+++ lib/excon/middleware/deadline.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Excon
+  module Errors
+    class DeadlineError < StandardError
+      def initialize(deadline, exceeded_by)
+        @deadline = deadline
+        @exceeded_by = exceeded_by
+      end
+
+      def to_s
+        "Deadline exceeded by #{@exceeded_by.abs}, \
+expected to have completed by #{@deadline.to_s(:iso8601)}"
+      end
+    end
+  end
+
+  module Middleware
+    class Deadline < Excon::Middleware::Base
+      def request_call(datum)
+        if datum[:deadline]
+          time_left = datum[:deadline] - Time.zone.now
+          unless time_left > 0
+            fail Errors::DeadlineError.new(datum[:deadline], time_left)
+          end
+          datum[:read_timeout] = time_left
+          datum[:write_timeout] = time_left
+        end
+
+        @stack.request_call(datum)
+      end
+    end
+  end
+end
