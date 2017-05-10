3ca9b212b30a9c95059f9d0f541f07392a5f76db
Yann Marquet <ymarquet@gmail.com>
2017-05-10 15:14:38 +0100

    Add service and configuration helpers for staff_nomis_checker_spec.rb (#812)
    * Add service and configuraton helpers
    
    Clean up staff nomis checker
    
    api disabled  #slot_availability_unknown?
    
    api disabled #errors_for
    
    remove all api_enabled
    
    use service and api spec feature helper
    
    group specs under API enabled/disabled
    
    Favor explicit moethod tests over redefining subject in every decribe blocks
    
    Refactor #prisoner_availability_unknown?
    
    Refactor contact_list_enabled?
    
    Make use of ENV vars in tests for clarity
    
    refactor #errors_for
    
    slot_errors and contact_list_unknown?
    
    Fix all of the specs
    
    * WIP Contatct list
    
    * Add missing spec for contact list
    
    * Fix the describe
    
    * switch subject(:instance) to just a subject
    
    * remove last instances of :instance
    
    * Rubocop fix

---
+++ spec/support/helpers/configuration_helpers.rb
@@ -0,0 +1,13 @@
+module ConfigurationHelpers
+  def switch_on(feature_flag)
+    switch_feature_flag_with(feature_flag, true)
+  end
+
+  def switch_off(feature_flag)
+    switch_feature_flag_with(feature_flag, false)
+  end
+
+  def switch_feature_flag_with(feature_flag, values)
+    allow(Rails.configuration).to receive(feature_flag).and_return(values)
+  end
+end
