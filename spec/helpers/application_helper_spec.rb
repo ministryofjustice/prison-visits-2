0778b98ac3789bce7d97617fc9cf6dc50b64a8c0
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2017-06-15 16:21:31 +0100

    Add test for i18n

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -46,4 +46,46 @@ RSpec.describe ApplicationHelper do
       end
     end
   end
+
+  describe 'javascript_i18n' do
+    it 'includes the days of the week' do
+      expect(javascript_i18n).to include(
+        days: %w[ Sunday Monday Tuesday Wednesday Thursday Friday Saturday ]
+      )
+    end
+
+    it 'includes the months of the year with January at index 0' do
+      # JavaScript follows the libc convention of 0 = January etc.
+      expect(javascript_i18n).to include(
+        months: %w[
+          January February March April May June
+          July August September October November December
+        ]
+      )
+    end
+
+    it 'includes abbreviated months of the year with January at index 0' do
+      # JavaScript follows the libc convention of 0 = January etc.
+      expect(javascript_i18n).to include(
+        abbrMonths: %w[ Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec ]
+      )
+    end
+
+    it 'includes am/pm' do
+      expect(javascript_i18n).to include(am: 'am', pm: 'pm')
+    end
+
+    it 'includes minutes and hours' do
+      expect(javascript_i18n).to include(
+        minute: {
+          one: 'min',
+          other: 'mins'
+        },
+        hour: {
+          one: 'hour',
+          other: 'hours'
+        }
+      )
+    end
+  end
 end
