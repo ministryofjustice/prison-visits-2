c93a5d9b162b81ae11c52dfd01ed4b1f73c70fc2
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-06-26 15:25:29 +0100

    merge two queries in one

--- app/controllers/api/prisons_controller.rb
+++ app/controllers/api/prisons_controller.rb
@@ -3,8 +3,10 @@ module Api
     def index
       # temporarily filter out ALI so no new visit
       # requests are sent to ALI
-      albany = Estate.find_by!(nomis_id: 'ALI')
-      @prisons = Prison.where.not(estate_id: albany.id).order(name: :asc).all
+      @prisons = Prison.joins(:estate).
+                   where.not(
+                     estates: { nomis_id: 'ALI' }
+                   ).order(name: :asc).all
     end
 
     def show
