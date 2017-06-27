898eed5cf3f1b5e35077acd02baa5c4dfaab82bb
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-06-27 17:13:23 +0100

    Send event Timing

--- app/services/google_api_client.rb
+++ app/services/google_api_client.rb
@@ -1,6 +1,3 @@
-require 'googleauth'
-require 'google/apis/analytics_v3'
-
 class GoogleApiClient
 
   ENDPOINT = URI.parse('https://www.google-analytics.com/collect').freeze
@@ -9,14 +6,15 @@ class GoogleApiClient
     self.web_property_id = Rails.application.config.ga_id
   end
 
-  def ga_event(user_agent, ip, cid)
+  def ga_event(user_agent, ip, user, prison, value)
+    data = payload_data(user_agent, ip, user, prison, value)
     client.post(
       path: ENDPOINT.path,
       headers: {
         'Content-Type' => 'application/x-www-form-urlencoded',
 
       },
-      body: URI.encode_www_form(payload_data(cid))
+      body: URI.encode_www_form(data)
     )
   end
 
@@ -24,17 +22,21 @@ class GoogleApiClient
   attr_accessor :client, :web_property_id
 
   def client
-    @client ||= Excon.new(ENDPOINT.host, persistent: true)
+    @client ||= Excon.new(ENDPOINT.to_s, persistent: true)
   end
 
-  def payload_data(cid, user_agent, ip)
+  def payload_data(user_agent, ip, user, prison, value)
     {
-      v: 1,
+      v:   1,
+      uip: ip,
       tid: web_property_id,
-      cid: cid,
-      t:   'visit_processed',
+      cid: SecureRandom.base64,
       ua:  user_agent,
-      uip: ip
+      t:   'timing',
+      utc: prison.name,
+      utv: 'Process',
+      utt: value,
+      utl: user.id
     }
   end
 
--- spec/services/google_api_client_spec.rb
+++ spec/services/google_api_client_spec.rb
@@ -1,10 +1,16 @@
 require "rails_helper"
 
+
+
 RSpec.describe GoogleApiClient do
 
-  let(:user_agent_string) { Faker::Internet.user_agent }
+  let(:user_agent) { 'some user agent string' }
+  let(:ip)         { FFaker::InternetSE.ip_v4_address  }
+  let(:user)       { create :user }
+  let(:prison)     { create :prison }
 
   it 'is able to authenticate', vcr: { cassette_name: 'google_analytics', record: :all } do
-    subject.ga_event user_agent
+    subject.ga_event user_agent, ip, user, prison, 3600
   end
+
 end
