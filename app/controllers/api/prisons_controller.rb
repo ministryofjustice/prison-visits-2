f833ae95849934010d86f0d24c64ed86f38dd821
Yann Marquet <ymarquet@gmail.com>
2017-06-28 11:15:19 +0100

    remove ALI prison filtering from api controller (#950)

--- app/controllers/api/prisons_controller.rb
+++ app/controllers/api/prisons_controller.rb
@@ -1,12 +1,7 @@
 module Api
   class PrisonsController < ApiController
     def index
-      # temporarily filter out ALI so no new visit
-      # requests are sent to ALI
-      @prisons = Prison.joins(:estate).
-                   where.not(
-                     estates: { nomis_id: 'ALI' }
-                   ).order(name: :asc).all
+      @prisons = Prison.order(name: :asc).all
     end
 
     def show
