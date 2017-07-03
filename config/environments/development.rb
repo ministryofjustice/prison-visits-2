625996aecd8931c564f2de913ed93c9ff1d6da67
yann marquet <ymarquet@gmail.com>
2017-07-03 09:17:06 +0100

    add wercker.yml with basic setup
    fix wercker
    
    set up postgres 9.4
    
    export RAILS_ENV
    
    devug pipeline
    
    fix RAILS_ENV export
    
    fix application.rb
    
    Fix wercker steps MOJ on build image is corrupted
    
    Wipe cache
    
    Remove buggy debug in application.rb :trollface:
    
    bundle jobs to 4
    
    remove wipe cache
    
    postgres connection
    
    append RAILS_ENV
    
    install browser dependencies
    
    start xvfb
    
    Push to pvbteam docker repo
    
    fix tag format
    
    rename rake build step
    
    tag build as latest
    
    set working directory
    
    try do override the entrypoint
    
    bypass tests
    
    Escape env var
    
    unquote working-dir value
    
    unquote all of the things
    
    Remove full path
    
    cmd vs entrypoint
    
    remove entrypoint and cmd
    
    speed up build
    
    try bare build
    
    Wait for postgres
    
    make run test runnable
    
    psql
    
    debug
    
    run service straight in wait for postgres
    
    debug host
    
    fix run-test
    
    fix run-test
    
    DEBUG env
    
    pgpassword
    
    debug run-test
    
    Fix script
    
    run app in script
    
    Fix postgres username in database.yml
    
    fix database.yml
    
    don't exec so the container stays alive
    
    change database YAML
    
    Fix run-test script
    
    run sideqik
    
    run in production mode
    
    actually export env
    
    remvoe garbage from datase.yaml
    
    remvoe garbage from datase.yaml
    
    serve static assets
    
    remove sidekiq from test config (will run in a separate container)
    
    fix pool to start puma
    
    don not deamonise puma
    
    Don't run server in production mode
    
    run tests
    
    fix run-test.sh wrapper
    
    use database url in tests
    
    fix wercker yaml
    
    Fix Wercker yml do db:create in run-test.sh
    
    fix rake task
    
    install gecko driver
    
    actually install gecko driver
    
    use DATABASE URL in development
    
    printenv
    
    add dev pipeline
    
    Fix Postgres username
    
    add run sidekiq test docker wrapper script
    
    runable sidekiq test wrapper
    
    Use custom pvbteam/ruby:2.4.1 docker image
    
    run tests
    
    Use SMTP settings if running integration tests on docker containers
    
    Fix rubocop errors
    
    tweak run-test.sh
    
    use for default_url_options PUBLIC_SERVICE_URL in developement mode
    
    This enable to run integration tests smoothly in a docker environment
    
    Fix rubocop warmings
    
    silence rubocop
    
    Make Link Directory use ENV variable if available in developement mode
    
    trigger integration tests pipeline
    
    run integration tests in separate pipeline
    
    fix trigger pipeline step
    
    trigger build
    
    only start services on dev and build pipeline
    
    attempt to fix trigger pipeline
    
    Fix pipeline trigger
    
    trigger build from pvb2
    
    run wercker integration test  branch
    
    Pass in Branch
    
    trigger build
    
    does this trigger a build?
    
    debug pipeline-trigger
    
    trigger correct branch build
    
    run integration tests - trigger pipeline
    
    increase timeout
    
    bumpe trigger pipeline step
    
    trigger build
    
    bump pipeline
    
    Bump trigger-pipeline steps
    
    trigger build
    
    trigger build
    
    re-instate production database settings
    
    add new line
    
    remove typo
    
    does not seem to pickup the host
    
    Use double quotes to content circle odd stripping of single quotes
    
    add after step to trigger build
    
    Fix URL
    
    Try curl verbose to debug request body
    
    remove commitHASH
    
    trigger a specific branch
    
    trigger new pipeline
    
    try to ignore branches on pipeline but trigger via api
    
    no custom dockerfile
    
    track pending integration tests
    
    Fix undifned variable
    
    Fix spelling mistakes
    
    don't trigger integration tests if build failed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,9 +5,27 @@ Rails.application.configure do
   config.eager_load = false
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.smtp_settings =
-    { address: 'localhost', port: 1025, domain: 'localhost' }
+
+  # check we use mailtrap
+  # usefull for testing email and integration tests
+  # which run in developement mode in wercker CI
+  if ENV['SMTP_DOMAIN'] == 'smtp.mailtrap.io'
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      user_name: ENV.fetch('SMTP_USERNAME'),
+      password:  ENV.fetch('SMTP_PASSWORD'),
+      address:   ENV.fetch('SMTP_HOSTNAME'),
+      port:      ENV.fetch('SMTP_PORT'),
+      domain:    ENV.fetch('SMTP_DOMAIN'),
+      authentication: :cram_md5
+    }
+  else
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.smtp_settings =
+      { address: 'localhost', port: 1025, domain: 'localhost' }
+  end
+
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.assets.debug = true
