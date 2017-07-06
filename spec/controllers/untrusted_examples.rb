80f9b8044c93d9e93adcc27f5e4be37bd22d62d2
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-07-06 09:59:58 +0100

    relpace do/end for `{}`

--- spec/controllers/untrusted_examples.rb
+++ spec/controllers/untrusted_examples.rb
@@ -1,6 +1,6 @@
 RSpec.shared_examples 'disallows untrusted ips' do
   context 'an untrusted ip' do
-    before do request.headers['REMOTE_ADDR'] = '192.168.1.0' end
+    before { request.headers['REMOTE_ADDR'] = '192.168.1.0' }
 
     it 'raises a not found error' do
       expect { subject }.to raise_error(ActionController::RoutingError)
