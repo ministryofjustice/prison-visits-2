a742ada8ed34c30c17cb08d52a3179c6dc403789
Yann Marquet <ymarquet@gmail.com>
2017-07-24 10:16:14 +0100

    Cast cookie value as Integer (#986)
    * Cast cookie value as Integer
    
    * remove happy path dup cassettes

--- spec/features/process_a_request_unprocessable_spec.rb
+++ spec/features/process_a_request_unprocessable_spec.rb
@@ -14,30 +14,28 @@ RSpec.feature 'Processing a request', js: true do
     scenario 'trying to double process a visit', vcr: { cassette_name: 'process_booking_happy_path' } do
       # VCR doesn't allow nesting the same cassette. Since we have to requests
       # to the process page we need to setup another happy path cassette.
-      VCR.use_cassette("process_booking_happy_path-dup") do
-        Capybara.using_session('window1') do
-          visit prison_visit_path(vst, locale: 'en')
+      Capybara.using_session('window1') do
+        visit prison_visit_path(vst, locale: 'en')
 
-          check 'Prisoner details are incorrect', visible: false
-        end
+        check 'Prisoner details are incorrect', visible: false
+      end
 
-        Capybara.using_session('window2') do
-          visit prison_visit_path(vst, locale: 'en')
+      Capybara.using_session('window2') do
+        visit prison_visit_path(vst, locale: 'en')
 
-          check 'Prisoner details are incorrect', visible: false
-        end
+        check 'Prisoner details are incorrect', visible: false
+      end
 
-        Capybara.using_session('window1') do
-          click_button 'Process'
+      Capybara.using_session('window1') do
+        click_button 'Process'
 
-          expect(page).to have_text('Thank you for processing the visit')
-        end
+        expect(page).to have_text('Thank you for processing the visit')
+      end
 
-        Capybara.using_session('window2') do
-          click_button 'Process'
+      Capybara.using_session('window2') do
+        click_button 'Process'
 
-          expect(page).to have_text("Visit can't be processed")
-        end
+        expect(page).to have_text("Visit can't be processed")
       end
     end
   end
