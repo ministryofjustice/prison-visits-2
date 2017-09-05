0e33a5269f4cf8e7edb97128da9e7eae63927102
Alan Kennedy <alankennedy100@gmail.com>
2017-09-05 10:15:35 +0100

    Normalise noms_id on offenders
    The offenders noms_id is used to call the location api which expects the value
    to be normalised.

--- app/services/nomis/offender.rb
+++ app/services/nomis/offender.rb
@@ -2,7 +2,10 @@ class Nomis::Offender
   include NonPersistedModel
 
   attribute :id
-  attribute :noms_id
+  attribute :noms_id,
+    String,
+    coercer: ->(number) { number&.upcase&.strip }
+
   validates_presence_of :id, :noms_id
 
   def api_call_successful?
