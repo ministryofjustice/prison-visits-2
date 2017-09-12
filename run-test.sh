8525eea6f2c72189b44950fe22b41ce3570845ca
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-09-12 17:49:01 +0100

    try circle setup for circleci-2

--- run-test.sh
+++ run-test.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 # wait-for-postgres.sh
 
+set -eo pipefail
 
-set -e
-export DATABASE_URL=postgres://$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT/moj_sso_development
-export PGPASSWORD=$POSTGRES_ENV_POSTGRES_PASSWORD
-export RAILS_SERVE_STATIC_FILES=true
-until psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -U "postgres" -c '\l'; do
-    >&2 echo "Postgres @ $POSTGRES_PORT_5432_TCP_ADDR is unavailable - sleeping"
-    sleep 1
-done
-
->&2 echo "Postgres is up - executing command"
-
-bin/rake db:setup
-bundle exec rails s -b 0.0.0.0 -p 3000
+RAILS_ENV=test bin/rake db:create
+xvfb-run bin/rake
