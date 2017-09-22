68efdd115be1b6398245de9b2cf10b467a151e08
Kath Pobee-Norris <kpobeenorris@gmail.com>
2017-09-22 09:09:44 +0100

    Refactor checkbox
    Refactor and improve test coverage

--- spec/decorators/rejection/reason_decorator_spec.rb
+++ spec/decorators/rejection/reason_decorator_spec.rb
@@ -1,21 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Rejection::ReasonDecorator do
-  let(:reasons) { ['reason one', 'reason two'] }
+  let(:reasons) { %w[reason_one reason_two] }
 
   describe "#checkbox_for" do
     let(:html_options) { {} }
+    let(:reason) { reasons.sample }
     let(:checkbox) do
       Capybara.string(
         described_class.decorate(reasons).checkbox_for(reason, html_options)
       )
     end
 
-    context 'when a reason is included' do
-      let(:reason) { reasons.sample }
+    it "has a base JS class to manage rejections" do
+      expect(checkbox).to have_css('.js-Rejection')
+    end
 
-      it "add classes to instruct the JS to show rejection messages upon selection" do
-        expect(checkbox).to have_css('.js-Rejection.js-restrictionOverride')
+    context 'when a reason is included' do
+      it "add the class and data attribute to instruct the JS to show rejection messages upon selection" do
+        expect(checkbox).to have_css(".js-restrictionOverride[data-override='#{reason.dasherize}']")
       end
 
       context 'html_options' do
@@ -31,7 +34,7 @@ RSpec.describe Rejection::ReasonDecorator do
       let(:reason) { 'reason_not_included' }
 
       it "does not have instruct the JS to show any rejection messages upon selection" do
-        expect(checkbox).not_to have_css('.js-Rejection.js-restrictionOverride')
+        expect(checkbox).not_to have_css('.js-restrictionOverride')
       end
     end
   end
