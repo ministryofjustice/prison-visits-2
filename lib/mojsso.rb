790e9e8d31e51a9b2ccdebae0ab711f38922781a
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-09-28 12:26:03 +0100

    remove hack needed as of omniauth 1.3.1
    callback_url bug fixed in 1.4.0

--- lib/mojsso.rb
+++ lib/mojsso.rb
@@ -27,24 +27,6 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get('/api/user_details').parsed
       end
-
-      # Without this login with sso breaks.
-      #
-      # Fix as suggested here: https://github.com/intridea/omniauth-oauth2/commit/26152673224aca5c3e918bcc83075dbb0659717f#commitcomment-19809835
-      # Other link about the issue: https://github.com/intridea/omniauth-oauth2/issues/81
-      #
-      # omniauth-oauth2 after version 1.3.1 it changed the way that the callback
-      # url gets generated. This new version doesn't match the redirect uri as set in
-      # SSO so login breaks (I think it doesn't strip the previous query string,
-      # although I would need to double check to be sure)
-      #
-      # The issue seems quite common among multiple SSO providers like Google,
-      # Facebook, Dropbox, etc
-      #
-      # Detailed information can be found on the comments of the above links.
-      def callback_url
-        full_host + script_name + callback_path
-      end
     end
   end
 end
