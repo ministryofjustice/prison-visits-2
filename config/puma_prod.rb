8dbc61f0d571feeaf31e18f0d6178e1c71b08e86
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-10-23 15:25:26 +0100

    tweak puma config

--- config/puma_prod.rb
+++ config/puma_prod.rb
@@ -1,4 +1,16 @@
 require 'yaml'
 db_pool = YAML.load_file('config/database.yml')['production']['pool']
-
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads 0, db_pool
+
+preload_app!
+
+rackup      DefaultRackup
+port        3000
+environment ENV['RACK_ENV'] || 'production'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
