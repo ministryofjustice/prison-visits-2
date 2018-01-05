329fcf8e15686e92776415ba287cb3deb3f2421e
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2018-01-05 11:20:38 +0000

    Disable some cops

--- app/controllers/prison/dashboards_controller.rb
+++ app/controllers/prison/dashboards_controller.rb
@@ -3,7 +3,6 @@ class Prison::DashboardsController < ApplicationController
 
   before_action :authorize_prison_request
   before_action :authenticate_user
-  before_action :verify_switch_estates, only: :switch_estates
 
   def inbox
     requested_visits(estates: current_estates)
