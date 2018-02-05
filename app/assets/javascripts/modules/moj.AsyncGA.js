5aa60dd08f7191bc58c533751e9576872061d0b9
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2018-02-05 10:42:32 +0000

    Add ga id

--- app/assets/javascripts/modules/moj.AsyncGA.js
+++ app/assets/javascripts/modules/moj.AsyncGA.js
@@ -4,13 +4,19 @@
 
   moj.Modules.AsyncGA = {
     el: '.js-AsyncGA',
+
     init: function() {
       GOVUK.Analytics.load();
+      if($(this.el).length>0){
+        this.render();
+      }
+    },
 
+    render: function(){
       // Use document.domain in dev, preview and staging so that tracking works
       // Otherwise explicitly set the domain as www.gov.uk (and not gov.uk).
       var cookieDomain = (document.domain === 'www.gov.uk') ? '.www.gov.uk' : document.domain;
-      var gaTrackingId = $(this.el).eq(0).data('ga-tracking-id');
+      var gaTrackingId = $(this.el).data('ga-tracking-id');
 
       // Configure profiles and make interface public
       // for custom dimensions, virtual pageviews and events
@@ -19,13 +25,13 @@
         cookieDomain: cookieDomain
       });
 
-      this.hitTypePage  = $(this.el).eq(0).data('hit-type-page');
+      this.hitTypePage  = $(this.el).data('hit-type-page');
       if (this.hitTypePage) {
-        GOVUK.analytics.trackPageview(location.pathname + '#' + this.hitTypePage);
+        GOVUK.analytics.trackPageview(this.hitTypePage);
       } else {
         GOVUK.analytics.trackPageview();
       }
-    },
+    }
   };
 
 }());
