6fabffe0116d3eec681b7ee9ac5ff9d784cb8c91
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2018-02-08 15:54:37 +0000

    Revert "Use the prisoner number type on Prisoner"
    This reverts commit 982ad1c72a36ea447669687ff35b49eec4179bc2.

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -2,8 +2,16 @@ class Prisoner < ActiveRecord::Base
   include Person
   extend FreshnessCalculations
 
-  attribute :number, :prisoner_number
-
   has_many :visits, dependent: :destroy
   validates :number, presence: true
+
+  before_validation(on: :create) do
+    if attribute_present?('number')
+      self.number = self.class.normalise_number(number)
+    end
+  end
+
+  def self.normalise_number(number)
+    number.upcase.strip
+  end
 end
