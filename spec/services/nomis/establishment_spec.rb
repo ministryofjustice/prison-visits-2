642390a04ee10f0baf72b5277dbdb27061ed1246
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2018-02-22 12:21:00 +0000

    Add housing location Value Object

--- spec/services/nomis/establishment_spec.rb
+++ spec/services/nomis/establishment_spec.rb
@@ -6,12 +6,20 @@ RSpec.describe Nomis::Establishment, type: :model do
       'establishment' =>
       {
         'code' => "ISI",
-        'desc' => "ISIS HMP/YOI"
+        'desc' => "ISIS HMP/YOI",
+        "housing_location" => {
+          "description" => "ISI-2-1-1",
+          "levels" => [
+            { "type" => "Wing",    "value" => "2" },
+            { "type" => "Landing", "value" => "1" },
+            { "type" => "Cell",    "value" => "1" }
+          ]
+        }
       }
     }
   end
 
-  subject { described_class.build(attributes) }
+  subject { described_class.new(attributes) }
 
   it { is_expected.to be_valid }
 
@@ -21,16 +29,12 @@ RSpec.describe Nomis::Establishment, type: :model do
 
   describe '.build' do
     context 'when internal location is not nested under the establishment key' do
-      before do
-        attributes['housing_location'] = "ISI-2-1-1"
-      end
-
       it 'builds the object correctly' do
         expect(subject).
           to have_attributes(
             code: "ISI",
             desc: "ISIS HMP/YOI",
-            housing_location: "ISI-2-1-1"
+            housing_location: instance_of(Nomis::HousingLocation)
              )
       end
     end
