6714d1bbb6dd920273982e3382e296f2bd7bb019
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2018-03-06 15:56:26 +0000

    Add guards for when NOMIS API is not enabled

--- app/presenters/prisoner_location.rb
+++ app/presenters/prisoner_location.rb
@@ -1,4 +1,5 @@
 class PrisonerLocation
+  NOT_LIVE = 'not_live'.freeze
   delegate :internal_location, to: :prisoner_location_validation
 
   def initialize(prisoner_location_validation)
@@ -6,6 +7,7 @@ class PrisonerLocation
   end
 
   def status
+    return NOT_LIVE unless Nomis::Api.enabled?
     return if prisoner_location_validation.valid?
     prisoner_location_validation.errors.full_messages.first
   end
