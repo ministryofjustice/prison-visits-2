56aa4e332dcae0a3945eda11720b91933181dd8b
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2018-04-12 11:48:17 +0100

    remove unnecessary object allocation

--- app/types/contacts_enumerable_type.rb
+++ app/types/contacts_enumerable_type.rb
@@ -1,7 +1,14 @@
 class ContactsEnumerableType < ActiveModel::Type::Value
   def cast(value)
-    contacts = value.map { |contact| ContactType.new.cast(contact) }
+    contacts = value.map { |contact| type_caster.cast(contact) }
 
     ContactsEnumerable.new(contacts)
   end
+
+private
+
+  def type_caster
+    @type_caster ||= ContactType.new
+  end
+
 end
