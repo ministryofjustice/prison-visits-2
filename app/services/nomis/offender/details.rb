739a1809c293ceaf6b95f32c66b52fd0a6731d6e
Yann Marquet <ymarquet@gmail.com>
2018-04-18 13:51:23 +0100

    Fix all missing attributes from offender details (#1614)

--- app/services/nomis/offender/details.rb
+++ app/services/nomis/offender/details.rb
@@ -3,22 +3,25 @@ module Nomis
     class Details
       include MemoryModel
 
-      attribute :given_name, :string
-      attribute :surname, :string
-      attribute :middle_names, :string
-      attribute :title, :string
-      attribute :nationalities, :string
-      attribute :date_of_birth, :date
       attribute :aliases
-      attribute :gender
-      attribute :religion
-      attribute :ethnicity
-      attribute :convicted, :boolean
-      attribute :imprisonment_status
-      attribute :cro_number, :string
-      attribute :pnc_number, :string
-      attribute :iep_level
       attribute :api_call_successful, :boolean, default: true
+      attribute :convicted, :boolean
+      attribute :cro_number, :string
+      attribute :csra
+      attribute :date_of_birth, :date
+      attribute :diet
+      attribute :ethnicity
+      attribute :gender
+      attribute :given_name, :string
+      attribute :iep_level
+      attribute :imprisonment_status
+      attribute :language
+      attribute :middle_names, :string
+      attribute :nationalities, :string
+      attribute :pnc_number, :string
+      attribute :religion
+      attribute :surname, :string
+      attribute :title, :string
 
       def valid?
         api_call_successful
