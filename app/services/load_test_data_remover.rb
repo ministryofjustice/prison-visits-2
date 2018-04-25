451883a30086c3f41cbdceb1389b9ba240a03f56
Toby Retallick <toby.retallick@gmail.com>
2018-04-25 09:51:09 +0100

    Sanitize SQL query

--- app/services/load_test_data_remover.rb
+++ app/services/load_test_data_remover.rb
@@ -1,6 +1,6 @@
 class LoadTestDataRemover
-  # Warning! Do not run on production. ENV['REMOVE_LOAD_TEST_DATA']
-  # should be set to 'true' on Staging env only.
+  # Warning! Do not run on production.
+  # ENV['REMOVE_LOAD_TEST_DATA'] should be set to 'true' on Staging env only.
   def self.run
     delete_load_test_data if Rails.configuration.remove_load_test_data
   end
@@ -11,14 +11,10 @@ class LoadTestDataRemover
     def delete_load_test_data
       visits = Visit.
         joins(:visitors).
-        where("visitors.first_name = 'Load'").
-        where("visitors.last_name = 'Test'")
+        where(visitors: { first_name: 'Load', last_name: 'Test' }
+      )
 
-      visits.each do |visit|
-        visit.prisoner.destroy
-      end
-
-      visits.destroy_all
+      visits.each { |visit| visit.prisoner.destroy }
     end
   end
 end
