dbcfadac70814ddde258ed3d6d9babd5a079027a
Toby Retallick <toby.retallick@gmail.com>
2018-04-26 09:45:21 +0100

    Pass in the first name and last name of the visitor as args

--- app/services/load_test_data_remover.rb
+++ app/services/load_test_data_remover.rb
@@ -1,8 +1,8 @@
 class LoadTestDataRemover
   # Warning! Do not run on production.
   # ENV['REMOVE_LOAD_TEST_DATA'] should be set to 'true' on Staging env only.
-  def self.run
-    delete_load_test_data if Rails.configuration.remove_load_test_data
+  def self.delete_visits_created_by(first_name, last_name)
+    run(first_name, last_name) if Rails.configuration.remove_load_test_data
   end
 
   class << self
@@ -12,12 +12,14 @@ class LoadTestDataRemover
       :prisoner, :visitors, :visit_state_changes, :messages, :rejection, :cancellation
     ]
 
-    def delete_load_test_data
+    def run(first_name, last_name)
       Visit.
         includes(ASSOCIATIONS).
-        where(visitors: { first_name: 'Load', last_name: 'Test' }).
+        where(visitors: { first_name: first_name, last_name: last_name }).
         find_in_batches(batch_size: 1000) do |batch|
-          batch.each { |visit|  LoadTestDataRemoverJob.perform_later(visit.prisoner) }
+          batch.each do |visit|
+            LoadTestDataRemoverJob.perform_later(visit.prisoner)
+          end
         end
     end
   end
