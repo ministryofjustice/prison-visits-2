7c9c176c1e7fb284d41ac94ed11de930e85b217b
Kath Pobee-Norris <kpobeenorris@gmail.com>
2018-05-16 15:48:16 +0100

    Update cookie expiry
    Up until this point there has been no expiry time for the staff (pvb2)
    cookie.  Following advice from the security team we have now set the
    cookie to expire after an hour.  The way that Rails manages the
    session_store means that active use of the site will keep extending the
    expiry time and will therefore only timeout once a user closes the
    browser, or doesn't use the site for an hour.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pvb2_session'
+Rails.application.config.session_store :cookie_store, expire_after: 1.hour,
+                                                      key: '_pvb2_session',
+                                                      secure: Rails.env.production?,
+                                                      httponly: true
