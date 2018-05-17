6a4db8c2b1cec2d4a9aff974a1e25da6f8738f2a
Kath Pobee-Norris <kpobeenorris@gmail.com>
2018-05-17 16:36:15 +0100

    Update cookie expiry

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pvb2_session'
+session_store_options = {}
+session_store_options[:key] = '_pvb2_session'
+session_store_options[:expire_after] = 1.hour unless Rails.env.test?
+
+Rails.application.config.session_store :cookie_store, session_store_options
