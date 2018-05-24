8773c8721282eb93171871540a89f06195616e92
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2018-05-24 15:30:44 +0100

    Use binstubs for puma and sidekiq

--- run-sidekiq.sh
+++ run-sidekiq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export RAILS_ENV=production
-bundle exec sidekiq --daemon -l /var/log/sidekiq.log --environment production
+bin/sidekiq --daemon -l /var/log/sidekiq.log --environment production
 
 tail -f /usr/src/app/log/logstash_production.json
