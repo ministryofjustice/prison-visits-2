b6ca6999671b091f1c382f76f64ac491e8a48d1d
Yann Marquet <ymarquet@gmail.com>
2018-06-18 15:31:08 +0100

    Increase statement timeout for migration (#1731)

--- db/migrate/20180531094251_add_creator_references_to_visit_state_changes.rb
+++ db/migrate/20180531094251_add_creator_references_to_visit_state_changes.rb
@@ -1,6 +1,6 @@
 class AddCreatorReferencesToVisitStateChanges < ActiveRecord::Migration[5.2]
 
-  set_statement_timeout(900)
+  set_statement_timeout(3600)
 
   def change
     add_reference :visit_state_changes, :creator, type: :uuid, polymorphic: true, index: true
