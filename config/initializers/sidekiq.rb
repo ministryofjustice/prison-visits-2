0e56c6ac432bc95ecf93f32d3c2fde928fdf9ec8
Toby Retallick <toby.retallick@gmail.com>
2019-02-28 10:00:18 +0000

    Create deploy scripts

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://redis.example.com:7372/0' }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://redis.example.com:7372/0' }
+  end
+end
