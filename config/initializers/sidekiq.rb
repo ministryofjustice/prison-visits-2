83f32f736011134b4db05ae7a94f7d78634c8ef0
Toby Retallick <toby.retallick@gmail.com>
2019-03-01 09:38:47 +0000

    Fix redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
-if Rails.env.production? && Rails.configuration.kubernetes_deployment?
+if Rails.env.production? && Rails.configuration.kubernetes_deployment
   Sidekiq.configure_server do |config|
-    config.redis = { url: "redis://#{Rails.configuration.redis_url}:6379", network_timeout: 5 }
+    config.redis = { url: "rediss://#{Rails.configuration.redis_url}:6379", network_timeout: 5, password: Rails.configuration.redis_password }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: "redis://#{Rails.configuration.redis_url}:6379", network_timeout: 5 }
+    config.redis = { url: "rediss://#{Rails.configuration.redis_url}:6379", network_timeout: 5, password: Rails.configuration.redis_password }
   end
 end
 
