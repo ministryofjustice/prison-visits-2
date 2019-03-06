3bb6e6aae87be03c8179da63841196aa36e5cb54
Toby Retallick <toby.retallick@gmail.com>
2019-03-06 14:20:04 +0000

    Fix kubernetes db connection
    Temporarily remove branch filters
    
    wip

--- config/initializers/kubernetes_database_config.rb
+++ config/initializers/kubernetes_database_config.rb
@@ -1,7 +1,3 @@
 if Rails.env.production? && Rails.configuration.kubernetes_deployment
-  ActiveRecord::Base.establish_connection(
-    adapter: 'postgresql',
-    url: ENV['DATABASE_URL'],
-    database: 'pvb2_production'
-  )
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 end
