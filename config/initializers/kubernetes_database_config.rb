ea16c3012efd3675b5f6ac12d643252a9b88c3d7
Toby Retallick <toby.retallick@gmail.com>
2019-03-06 12:25:45 +0000

    Set database adapter

--- config/initializers/kubernetes_database_config.rb
+++ config/initializers/kubernetes_database_config.rb
@@ -1,5 +1,6 @@
 if Rails.env.production? && Rails.configuration.kubernetes_deployment
   ActiveRecord::Base.establish_connection(
+    adapter: 'postgresql',
     url: ENV['DATABASE_URL'],
     database: 'pvb2_production'
   )
