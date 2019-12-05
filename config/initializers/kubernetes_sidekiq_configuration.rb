c60d0cb12fb9c9d1d147171f5f0d6855dbf1e671
Kath Pobee-Norris <kpobeenorris@gmail.com>
2019-12-05 09:08:47 +0000

    Appease the linter

--- config/initializers/kubernetes_sidekiq_configuration.rb
+++ config/initializers/kubernetes_sidekiq_configuration.rb
@@ -2,14 +2,14 @@ if Rails.env.production?
   Sidekiq.configure_server do |config|
     config.redis = {
       url: Rails.configuration.redis_url.to_s,
-      network_timeout: 5,
+      network_timeout: 5
     }
   end
 
   Sidekiq.configure_client do |config|
     config.redis = {
       url: Rails.configuration.redis_url.to_s,
-      network_timeout: 5,
+      network_timeout: 5
     }
   end
 end
