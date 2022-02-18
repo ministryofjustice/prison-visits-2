99c1852840eccaa49f56f1009b6e9f39df4ca186
georgeschena <georgeschena@hotmail.com>
2022-02-18 13:26:40 +0000

    Comment out GOV Notify work for now

--- app/services/gov_notify_emailer.rb
+++ app/services/gov_notify_emailer.rb
@@ -1,22 +1,22 @@
-require 'notifications/client'
+# require 'notifications/client'
 
-class GovNotifyEmailer
-  def initialize
-    @client = Notifications::Client.new(ENV['GOV_NOTIFY_API_KEY'])
-  end
+# class GovNotifyEmailer
+#   def initialize
+#     @client = Notifications::Client.new(ENV['GOV_NOTIFY_API_KEY'])
+#   end
 
-  def send_email(visit, prison)
-    @client.send_email(
-      email_address: visit.contact_email_address,
-      template_id: 'f63f6d5a-7c11-41f8-9110-ac9f47f19d6f',
-      personalisation: {
-        first_name: 'John Smith',
-        prison_name: prison.name,
-        reference_number: visit.reference_no || '',
-        slot_option_0: visit.slot_option_0 || '',
-        slot_option_1:  visit.slot_option_1 || '',
-        slot_option_2:  visit.slot_option_2 || ''
-      }
-    )
-  end
-end
+#   def send_email(visit, prison)
+#     @client.send_email(
+#       email_address: visit.contact_email_address,
+#       template_id: 'f63f6d5a-7c11-41f8-9110-ac9f47f19d6f',
+#       personalisation: {
+#         first_name: 'John Smith',
+#         prison_name: prison.name,
+#         reference_number: visit.reference_no || '',
+#         slot_option_0: visit.slot_option_0 || '',
+#         slot_option_1:  visit.slot_option_1 || '',
+#         slot_option_2:  visit.slot_option_2 || ''
+#       }
+#     )
+#   end
+# end
