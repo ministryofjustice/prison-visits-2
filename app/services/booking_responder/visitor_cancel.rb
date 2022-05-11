c0a2d99d6dc0c850203444e83cd6de4a3859b95f
georgeschena <georgeschena@hotmail.com>
2022-05-11 14:04:05 +0100

    Add cancel email

--- app/services/booking_responder/visitor_cancel.rb
+++ app/services/booking_responder/visitor_cancel.rb
@@ -7,6 +7,12 @@ class BookingResponder
                              reasons: [Cancellation::VISITOR_CANCELLED],
                              nomis_cancelled: false)
 
+        template_id = '12969e6f-96b4-40a6-994c-14432e604965'
+        @cancellation = visit.cancellation.decorate
+
+        @gov_notify_email = GovNotifyEmailer.new
+        @gov_notify_email.send_email(visit, template_id)
+
         BookingResponse.successful
       end
     end
