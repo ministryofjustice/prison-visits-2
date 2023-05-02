3923ef0a2b1c12346a68aa4b0cdb531a91b79621
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2023-05-02 14:36:25 +0100

    VB-2202, first draft anon batch update

--- app/services/depersonalizer.rb
+++ app/services/depersonalizer.rb
@@ -3,14 +3,14 @@ class Depersonalizer
   DATE = Date.new(1, 1, 1)
 
   def remove_personal_information(cutoff_date)
-    Prisoner.older_than(cutoff_date).update_all \
+    Prisoner.older_than(cutoff_date).only_non_anonymised(first_name).update_all \
       first_name: STRING, last_name: STRING,
       number: STRING, date_of_birth: DATE
 
-    Visitor.older_than(cutoff_date).update_all \
+    Visitor.older_than(cutoff_date).only_non_anonymised(first_name).update_all \
       first_name: STRING, last_name: STRING, date_of_birth: DATE
 
-    Visit.older_than(cutoff_date).update_all \
+    Visit.older_than(cutoff_date).only_non_anonymised(contact_email_address).update_all \
       contact_email_address: STRING, contact_phone_no: STRING
   end
 end
