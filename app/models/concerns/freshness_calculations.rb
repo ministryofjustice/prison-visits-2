8e86893bfc96f86deb33d38ecdcb39e57bf95b31
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2023-05-02 14:59:52 +0100

    Definition changes

--- app/models/concerns/freshness_calculations.rb
+++ app/models/concerns/freshness_calculations.rb
@@ -3,11 +3,11 @@ module FreshnessCalculations
     where(arel_table[:created_at].lt(date))
   end
 
-  def only_non_anon_name()
+  def only_non_anon_name
     where(arel_table[:first_name].not_eq('REMOVED'))
   end
 
-  def only_non_anon_email()
+  def only_non_anon_email
     where(arel_table[:contact_email_address].not_eq('REMOVED'))
   end
 end
