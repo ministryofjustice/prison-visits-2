0940ab4a542e420b83c2470544e8bfda9fa46419
Serafeim Maroulis <serafeim@hey.com>
2023-10-17 10:07:10 +0100

    Adapt Excon middleware for Ruby 3.0
    * Excon bumped to 0.104.0 and the monkey patches had to be refactored.
    
    * It was previously locked to 0.62.0 which was not compatible with Ruby
    3.0 and the app was raising:
    
    ArgumentError:
      tried to create Proc object without a block

--- spec/lib/excon/middleware/custom_idempotent_spec.rb
+++ spec/lib/excon/middleware/custom_idempotent_spec.rb
@@ -33,6 +33,7 @@ RSpec.describe Excon::Middleware::CustomIdempotent do
 
   it "Idempotent request with a timeout error" do
     WebMock.stub_request(:get, /\w/).to_timeout
+
     expect { connection.request(method: :get, path: '/some-path') }.
       to raise_error(Excon::Errors::Timeout)
   end
@@ -154,5 +155,137 @@ RSpec.describe Excon::Middleware::CustomIdempotent do
 
     response = connection.request(method: :get, idempotent: true, path: '/some-path', retry_limit: 6)
     expect(response.status).to eq(200)
+  end
+
+
+  it "Retry limit and sleep in constructor with socket erroring first 2 times" do
+    run_count = 0
+    WebMock.stub_request(:get, /\w/).and_return(
+      lambda do |request|
+        run_count += 1
+        if run_count <= 2 # First 5 calls fail.
+          raise Excon::Error::Socket.new(Exception.new "Mock Error")
+        else
+          {body: request.body, headers: request.headers, status: 200}
+        end
+      end
+    )
+
+    # NOTE: A short :retry_interval will avoid slowing down the tests.
+    response = connection.request(method: :get, idempotent: true, path: '/some-path', retry_limit: 3, retry_interval: 0.1)
+    expect(response.status).to eq(200)
+  end
+
+  it "Retry limit and sleep in constructor with socket erroring first 2 times" do
+    run_count = 0
+    WebMock.stub_request(:get, /\w/).and_return(
+      lambda do |request|
+        run_count += 1
+        if run_count <= 2 # First 5 calls fail.
+          raise Excon::Error::Socket.new(Exception.new "Mock Error")
+        else
+          {body: request.body, headers: request.headers, status:  200}
+        end
+      end
+    )
+
+    # NOTE: A short :retry_interval will avoid slowing down the tests.
+    expect {
+      connection.request(method: :get, idempotent: true, path: '/some-path', retry_limit: 2, retry_interval: 0.1)
+    }.to raise_error(Excon::Error::Socket)
+  end
+
+  it "Idempotent request with custom error first 3 times" do
+    run_count = 0
+    WebMock.stub_request(:get, /\w/).and_return(
+      lambda do |request|
+        run_count += 1
+        if run_count <= 3 # First 3 calls fail.
+          raise "oops"
+        else
+          {body: request.body, headers: request.headers, status: 200}
+        end
+      end
+    )
+
+    response = connection.request(method: :get, :idempotent => true, :retry_errors => [RuntimeError], :path => '/some-path')
+    expect(response.status).to eq(200)
+  end
+
+  it("Idempotent request with custom error first 5 times") do
+    run_count = 0
+    WebMock.stub_request(:get, /\w/).and_return(
+      lambda do |request|
+        run_count += 1
+        if run_count <= 5 # First 5 calls fail.
+          raise "oops"
+        else
+          {body: request.body, headers: request.headers, status: 200}
+        end
+      end
+    )
+
+    expect {
+      connection.request(method: :get, idempotent: true, retry_errors: [RuntimeError], path: '/some-path')
+    }.to raise_error(RuntimeError)
+  end
+
+  it "Overriding default retry_errors" do
+    WebMock.stub_request(:get, /\w/).to_raise(
+      Excon::Error::Socket.new(Exception.new "Mock Error")
+    )
+
+    expect {
+      connection.request(:method => :get, :idempotent => true, :retry_errors => [RuntimeError], :path => '/some-path')
+    }.to raise_error(Excon::Error::Socket)
+  end
+
+  class Block
+    attr_reader :rewound
+
+    def initialize
+      @rewound = false
+    end
+
+    def call(_)
+    end
+
+    def rewind
+      @rewound = true
+    end
+  end
+
+  it "request_block rewound" do
+    run_count = 0
+    WebMock.stub_request(:get, /\w/).and_return(
+      lambda do |request|
+        run_count += 1
+        if run_count <= 1 # First call fails.
+          raise Excon::Error::Socket.new(Exception.new "Mock Error")
+        else
+          {body: request.body, headers: request.headers, status: 200}
+        end
+      end
+    )
+    request_block = Block.new
+    connection.request(method: :get, idempotent: true, path: '/some-path', request_block: request_block, retry_limit: 2, retry_interval: 0.1)
+    expect(request_block.rewound).to eq(true)
+  end
+
+  it "response_block rewound" do
+    run_count = 0
+    WebMock.stub_request(:get, /\w/).and_return(
+      lambda do |request|
+        run_count += 1
+        if run_count <= 1 # First call fails.
+          raise Excon::Error::Socket.new(Exception.new "Mock Error")
+        else
+          {body: request.body, headers: request.headers, status: 200}
+        end
+      end
+    )
+    response_block = Block.new
+    connection.request(method: :get, idempotent: true, path: '/some-path', response_block: response_block, retry_limit: 2, retry_interval: 0.1)
+    expect(response_block.rewound).to eq(true)
   end
 end
