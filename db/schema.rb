d390419216b72f442f8f8575afee1346a72753ee
Serafeim Maroulis <serafeim@hey.com>
2023-11-03 17:00:48 +0000

    Add Rails 6.1 default migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_10_102352) do
+ActiveRecord::Schema.define(version: 2023_11_03_165850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
