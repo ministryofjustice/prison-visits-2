5ea7f2a41f244fda1ebd94d633ddabed0ab7b84a
Serafeim Maroulis <serafeim@hey.com>
2023-11-08 10:03:40 +0000

    Enable Jasmine monkey patch only in dev, test

--- config/initializers/jasmine/asset_expander.rb
+++ config/initializers/jasmine/asset_expander.rb
@@ -1,14 +1,18 @@
-module Jasmine
-  class AssetExpander
-  private
+# https://github.com/jasmine/jasmine-gem/blob/v3.9.2/lib/jasmine/asset_expander.rb
 
-    def asset_bundle
-      return Rails4Or5Or6AssetBundle.new if Jasmine::Dependencies.rails4? ||
-        Jasmine::Dependencies.rails5? ||
-        Jasmine::Dependencies.rails6? ||
-        Jasmine::Dependencies.rails7?
+unless Rails.env.production?
+  module Jasmine
+    class AssetExpander
+      private
 
-      raise UnsupportedRailsVersion, 'Jasmine only supports the asset pipeline for Rails 4. - 6'
+      def asset_bundle
+        return Rails4Or5Or6AssetBundle.new if Jasmine::Dependencies.rails4? ||
+          Jasmine::Dependencies.rails5? ||
+          Jasmine::Dependencies.rails6? ||
+          Jasmine::Dependencies.rails7?
+
+        raise UnsupportedRailsVersion, 'Jasmine only supports the asset pipeline for Rails 4. - 7'
+      end
     end
   end
 end
