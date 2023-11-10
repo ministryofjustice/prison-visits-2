5689b80110cda8f97d4e10431cdfff7dce3691db
Serafeim Maroulis <serafeim@hey.com>
2023-11-10 10:43:33 +0000

    Enable the rest of Rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -119,5 +119,9 @@ module PrisonVisits
 
     # We still use ie stylesheets as well as the govuk_template
     config.action_view.preload_links_header = false
+
+    # Rails 7 new defaults
+    config.active_support.cache_format_version = 7.0
+    config.active_support.disable_to_s_conversion = true
   end
 end
