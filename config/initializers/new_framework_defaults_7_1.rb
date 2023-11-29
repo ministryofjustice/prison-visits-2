127d3b4ae4663b74d3bc2d5c0c282d16efe018b7
Serafeim Maroulis <serafeim@hey.com>
2023-11-29 14:00:39 +0000

    Set active_record.run_commit_callbacks_on_first_saved_instances_in_transaction = false
    Rails 7.1 default

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -65,7 +65,7 @@
 # state which matches what was committed to the database, typically the last
 # instance to save.
 #++
-# Rails.application.config.active_record.run_commit_callbacks_on_first_saved_instances_in_transaction = false
+Rails.application.config.active_record.run_commit_callbacks_on_first_saved_instances_in_transaction = false
 
 ###
 # Configures SQLite with a strict strings mode, which disables double-quoted string literals.
