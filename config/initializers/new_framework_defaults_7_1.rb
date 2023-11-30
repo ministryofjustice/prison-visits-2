265abb3dc3456672fed30ec2b0101d0b4c23d21c
Serafeim Maroulis <serafeim@hey.com>
2023-11-30 10:45:59 +0000

    Set active_record.run_after_transaction_callbacks_in_order_defined = true
    Rails 7.1 default

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -214,7 +214,7 @@ Rails.application.config.active_job.use_big_decimal_serializer = true
 # This matches the behaviour of all other callbacks.
 # In previous versions of Rails, they ran in the inverse order.
 #++
-# Rails.application.config.active_record.run_after_transaction_callbacks_in_order_defined = true
+Rails.application.config.active_record.run_after_transaction_callbacks_in_order_defined = true
 
 ###
 # Whether a `transaction` block is committed or rolled back when exited via `return`, `break` or `throw`.
