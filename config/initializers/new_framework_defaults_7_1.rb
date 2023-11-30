0b05d1df7697949213b282c59d320bb67ec9ac43
Serafeim Maroulis <serafeim@hey.com>
2023-11-30 10:13:24 +0000

    Set max log size for local
    Rails 7.1 default

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -159,9 +159,9 @@ Rails.application.config.active_job.use_big_decimal_serializer = true
 # `config.load_defaults 7.1` does not set this value for environments other than
 # development and test.
 #++
-# if Rails.env.local?
-#   Rails.application.config.log_file_size = 100 * 1024 * 1024
-# end
+if Rails.env.local?
+  Rails.application.config.log_file_size = 100 * 1024 * 1024
+end
 
 ###
 # Enable raising on assignment to attr_readonly attributes. The previous
