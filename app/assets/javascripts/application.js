7591ee7bd4240d876ebb105fc388faeb607d27f5
hutcheonb-moj <106151419+hutcheonb-moj@users.noreply.github.com>
2025-02-17 11:13:05 +0000

    13/02/25 bundle update (#3763)
    * 13/02/25 bundle update
    
    * unlimit notifications-ruby-client
    
    * Restrict ruby version
    
    * Jquery-ui-rails 7.0.0
    
    * Fix datepicker unable to find file
    
    * Bump sidekiq
    
    * add business hours only rule to dev/staging

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery-ui-autocomplete
 //= require modernizr-custom
 //= require dest/respond.min
-//= require jquery-ui/datepicker
+//= require jquery-ui/widgets/datepicker
 
 // Raven / Sentry
 //= require modules/moj.sentry
