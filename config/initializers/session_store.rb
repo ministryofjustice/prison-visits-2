9514c8fbafdabc9977d73f31a4d2fb8bff2cafb6
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2026-03-23 10:19:08 +0000

    bump cookie key name'

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 session_store_options = {}
-session_store_options[:key] = '_pvb2_session'
+session_store_options[:key] = '_pvb2'
 session_store_options[:expire_after] = 1.hour unless Rails.env.test?
 
 Rails.application.config.session_store :cookie_store, **session_store_options
