c6c4b69df825153ce361c8a0642bce22e575e8b8
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2026-03-23 16:01:34 +0000

    rubocop

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :active_record_store,
-                                       key: '_pvb_staff_session',
-                                       expire_after: 1.hour unless Rails.env.test?
+unless Rails.env.test?
+  Rails.application.config.session_store :active_record_store,
+                                         key: '_pvb_staff_session',
+                                         expire_after: 1.hour
+end
